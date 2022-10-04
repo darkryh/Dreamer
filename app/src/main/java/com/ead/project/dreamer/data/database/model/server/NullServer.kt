0d22f1d44932e076082084d6d1a36903f08f50b5
xavier <alex_torres-xc@hotmail.com>
2022-10-04 17:18:18 -0500

    update server.

--- app/src/main/java/com/ead/project/dreamer/data/database/model/server/NullServer.kt
+++ app/src/main/java/com/ead/project/dreamer/data/database/model/server/NullServer.kt
@@ -2,4 +2,4 @@ package com.ead.project.dreamer.data.database.model.server
 
 import com.ead.project.dreamer.data.database.model.Server
 
-class NullServer (var url : String) : Server()
+class NullServer (embeddedUrl:String) : Server(embeddedUrl)
