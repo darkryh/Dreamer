d3405aff96793233f81d8594b5ba504fc322229b
xavier <alex_torres-xc@hotmail.com>
2022-10-05 12:04:08 -0500

    update in functions.

--- app/src/main/java/com/ead/project/dreamer/data/utils/PatternManager.kt
+++ app/src/main/java/com/ead/project/dreamer/data/utils/PatternManager.kt
@@ -6,24 +6,23 @@ class PatternManager {
 
     companion object {
 
-        fun sliceReference(url : String): String? {
-
-            val regex = "([vf])([/=])(.+)([/&])?"
-            val pattern = Pattern.compile(regex)
-            val matcher = pattern.matcher(url)
-            return if (matcher.find()) {
-                matcher.group(3)?.replace("[&/]", "")
-            }
-            else null
-        }
-
-
-        fun variableReference(url: String,regex : String,groupIndex : Int = 1): String? {
+        fun singleMatch(url: String, regex : String, groupIndex : Int = 1): String? {
             val pattern = Pattern.compile(regex, Pattern.MULTILINE)
             val matcher = pattern.matcher(url)
             return if (matcher.find()) {
                 matcher.group(groupIndex)
             } else null
+        }
+
+        fun multipleMatches(string: String, regex: String, groupIndex: Int=1): List<String> {
+            val pattern = Pattern.compile(regex, Pattern.MULTILINE)
+            val matcher = pattern.matcher(string)
+            val stringArrayList = ArrayList<String>()
+            while (matcher.find()) {
+                matcher.group(groupIndex)?.let { stringArrayList.add(it) }
+            }
+            if (stringArrayList.isEmpty()) throw RuntimeException("null")
+            return stringArrayList
         }
     }
 }
