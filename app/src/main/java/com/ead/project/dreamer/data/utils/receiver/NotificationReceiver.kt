620926af50196c0457f1e9e7aae4b4748d1462a3
xavier <alex_torres-xc@hotmail.com>
2022-10-05 12:03:38 -0500

    NotificationReceiver update for custom notification about new series.

--- app/src/main/java/com/ead/project/dreamer/data/utils/receiver/NotificationReceiver.kt
+++ app/src/main/java/com/ead/project/dreamer/data/utils/receiver/NotificationReceiver.kt
@@ -5,21 +5,40 @@ import android.content.Context
 import android.content.Intent
 import com.ead.project.dreamer.data.commons.Constants
 import com.ead.project.dreamer.data.utils.DataStore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class NotificationReceiver : BroadcastReceiver() {
 
     companion object {
-        const val NOTIFICATION_ACTION = "NOTIFICATION_ACTION"
-        const val PREFERENCE_DEACTIVATION = 0
+        const val PREFERENCE_DEACTIVATION = "PREFERENCE_DEACTIVATION"
+        const val PREFERENCE_ACTIVATION_FAVORITES = "PREFERENCE_ACTIVATION_FAVORITES"
+        const val PREFERENCE_ACTIVATION_ALL = "PREFERENCE_ACTIVATION_ALL"
+        const val NOTIFICATION_ID = "123"
     }
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        when(intent?.extras?.getInt(NOTIFICATION_ACTION)) {
-            PREFERENCE_DEACTIVATION -> {
-                DataStore.writeBoolean(Constants.PREFERENCE_NOTIFICATIONS,false)
+    private val scope = CoroutineScope(SupervisorJob())
+    private val notifier = DreamerNotifier().notificationManager()
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        val pendingResult: PendingResult = goAsync()
+        scope.launch(Dispatchers.Default) {
+            try {
+                val id = intent?.getIntExtra(NOTIFICATION_ID, -1) ?: -1
+                when(intent?.action) {
+                    PREFERENCE_DEACTIVATION -> DataStore.writeIntAsync(Constants.PREFERENCE_NOTIFICATIONS,0)
+                    PREFERENCE_ACTIVATION_FAVORITES -> DataStore.writeIntAsync(Constants.PREFERENCE_NOTIFICATIONS,1)
+                    PREFERENCE_ACTIVATION_ALL -> DataStore.writeIntAsync(Constants.PREFERENCE_NOTIFICATIONS,2)
+                }
+                notifier.cancel(id)
+            } finally {
+                // Must call finish() so the BroadcastReceiver can be recycled
+                pendingResult.finish()
             }
         }
-    }
 
+    }
 
 }
