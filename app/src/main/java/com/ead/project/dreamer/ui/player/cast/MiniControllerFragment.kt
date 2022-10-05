74da37960ba7422862ea6aca24a14d56f6c0d6a4
xavier <alex_torres-xc@hotmail.com>
2022-10-05 12:02:31 -0500

    new update in the father class. MiniControllerFragment.java

--- app/src/main/java/com/ead/project/dreamer/ui/player/cast/MiniControllerFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/ui/player/cast/MiniControllerFragment.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ead.project.dreamer.data.utils.ui.DreamerUIController
-import com.google.android.gms.cast.framework.media.widget.MiniControllerFragment
+import android.widget.ImageView
+import com.ead.project.dreamer.data.utils.media.CastManager
+import com.ead.project.dreamer.data.utils.ui.DreamerUIMiniController
+import com.ead.project.dreamer.ui.player.cast.controller.MiniControllerFragment
+
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MiniControllerFragment : MiniControllerFragment() {
 
+    private var castManager : CastManager = CastManager(true)
+    private lateinit var dreamerUIMiniController: DreamerUIMiniController
+    private lateinit var customButtonView : ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = super.onCreateView(inflater, container, savedInstanceState)
-        val customButtonView = getButtonImageViewAt(1)
-        val myCustomUiController = DreamerUIController(customButtonView)
-        //uiMediaController?.bindViewToUIController(customButtonView, myCustomUiController)
+        customButtonView = getButtonImageViewAt(2)
+        dreamerUIMiniController = DreamerUIMiniController(customButtonView,castManager)
+        uiMediaController?.bindViewToUIController(customButtonView, dreamerUIMiniController)
         return view
     }
+
 }
