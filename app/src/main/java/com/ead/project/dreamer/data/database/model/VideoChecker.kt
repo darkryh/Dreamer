3ab28a5dc91cecd07087b9cf297d9c137cc0222a
xavier <alex_torres-xc@hotmail.com>
2022-10-05 10:56:49 -0500

    update adding downloading case.

--- app/src/main/java/com/ead/project/dreamer/data/database/model/VideoChecker.kt
+++ app/src/main/java/com/ead/project/dreamer/data/database/model/VideoChecker.kt
@@ -2,24 +2,25 @@ package com.ead.project.dreamer.data.database.model
 
 import com.ead.project.dreamer.data.commons.Constants
 import com.ead.project.dreamer.data.utils.DataStore
-import org.jsoup.Connection
-import org.jsoup.Jsoup
+import com.ead.project.dreamer.data.utils.ServerManager
+import java.net.HttpURLConnection
+import java.net.URL
 import javax.inject.Inject
 
 class VideoChecker @Inject constructor() {
 
     companion object {
 
-        fun getSorterServerList(list: List<String>) : MutableList<String> {
+        fun getSorterServerList(list: List<String>,isDownload : Boolean = false) : MutableList<String> {
             val pairList : MutableList<Pair<Int,String>> = ArrayList()
-            if (!DataStore.readBoolean(Constants.PREFERENCE_EXTERNAL_PLAYER))
-                for (data in list) {
-                    pairList.add(Pair(getPositions(data),data))
-                }
-            else
-                for (data in list) {
-                    pairList.add(Pair(getPositionsExternal(data),data))
-                }
+            if (!isDownload) {
+                if (!DataStore.readBoolean(Constants.PREFERENCE_EXTERNAL_PLAYER))
+                    for (data in list) pairList.add(Pair(getPositions(data),data))
+                else
+                    for (data in list) pairList.add(Pair(getPositionsExternal(data),data))
+            }
+            else for (data in list) pairList.add(Pair(getDownloadsPosition(data),data))
+
 
             pairList.sortBy { it.first }
             return pairList.map {
@@ -29,58 +30,90 @@ class VideoChecker @Inject constructor() {
 
         fun getConnection(url : String) : Boolean {
             return try {
-                when (Jsoup.connect(url)
-                    .ignoreContentType(true)
-                    .method(Connection.Method.GET)
-                    .execute()
-                    .statusCode()) {
+                val urlObject = URL(url)
+                val connection: HttpURLConnection = urlObject.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connect()
+                val code = connection.responseCode
+                connection.disconnect()
+                when (code) {
                     200 -> true
                     else -> false
                 }
-            } catch (e : Exception) {
-                e.printStackTrace()
-                false
+            } catch (e : Exception) { false }
+        }
+
+        private fun getDownloadsPosition(data: String) : Int {
+            return when (ServerManager.identify(data)) {
+                Constants.TITLE_ONEFICHIER -> 0
+                Constants.TITLE_MEDIAFIRE -> 1
+                Constants.TITLE_OKRU -> 2
+                Constants.TITLE_FEMBED -> 3
+                Constants.TITLE_SENDVID -> 4
+                Constants.TITLE_VOE -> 5
+                Constants.TITLE_ZIPPYSHARE -> 6
+                Constants.TITLE_BAYFILES -> 7
+                Constants.TITLE_STREAMTAPE -> 8
+                Constants.TITLE_SOLIDFILES -> 9
+                Constants.TITLE_PUJ -> 10
+                Constants.TITLE_VIDEOBIN -> 11
+                Constants.TITLE_STREAMSB -> 12
+                Constants.TITLE_GOOGLE_DRIVE -> 13
+                Constants.TITLE_ANONFILE -> 14
+                Constants.TITLE_FIRELOAD -> 15
+                Constants.TITLE_MP4UPLOAD -> 16
+                Constants.TITLE_UQLOAD -> 17
+                Constants.TITLE_MEGA -> 18
+                else -> 10000
             }
         }
 
         private fun getPositions(data : String) : Int {
-            return when (Server.identify(data)) {
-                Constants.TITLE_OKRU -> 0
-                Constants.TITLE_ONEFICHIER -> 1
-                Constants.TITLE_VOE -> 2
-                Constants.TITLE_FEMBED -> 3
-                Constants.TITLE_SOLIDFILES -> 4
-                Constants.TITLE_BAYFILES -> 5
-                Constants.TITLE_SENDVID -> 6
-                Constants.TITLE_STREAMTAPE -> 7
-                Constants.TITLE_ZIPPYSHARE -> 8
-                Constants.TITLE_PUJ -> 9
-                Constants.TITLE_EMBED -> 10
-                Constants.TITLE_VIDEOBIN -> 11
-                Constants.TITLE_MP4UPLOAD -> 12
-                Constants.TITLE_UQLOAD -> 13
-                Constants.TITLE_MEGA -> 14
+            return when (ServerManager.identify(data)) {
+                Constants.TITLE_MEDIAFIRE -> 0
+                Constants.TITLE_OKRU -> 1
+                Constants.TITLE_ONEFICHIER -> 2
+                Constants.TITLE_STREAMSB -> 3
+                Constants.TITLE_SENDVID -> 4
+                Constants.TITLE_VIDEOBIN -> 5
+                Constants.TITLE_FEMBED -> 6
+                Constants.TITLE_VOE -> 7
+                Constants.TITLE_BAYFILES -> 8
+                Constants.TITLE_ZIPPYSHARE -> 9
+                Constants.TITLE_STREAMTAPE -> 10
+                Constants.TITLE_SOLIDFILES -> 11
+                Constants.TITLE_PUJ -> 12
+                Constants.TITLE_GOOGLE_DRIVE -> 13
+                Constants.TITLE_ANONFILE -> 14
+                Constants.TITLE_FIRELOAD -> 15
+                Constants.TITLE_MP4UPLOAD -> 16
+                Constants.TITLE_UQLOAD -> 17
+                Constants.TITLE_MEGA -> 18
                 else -> 10000
             }
         }
 
         private fun getPositionsExternal(data : String) : Int {
-            return when (Server.identify(data)) {
-                Constants.TITLE_FEMBED -> 0
+            return when (ServerManager.identify(data)) {
+                Constants.TITLE_MEDIAFIRE -> 0
                 Constants.TITLE_ONEFICHIER -> 1
-                Constants.TITLE_VOE -> 2
-                Constants.TITLE_OKRU -> 3
-                Constants.TITLE_SOLIDFILES -> 4
-                Constants.TITLE_BAYFILES -> 5
-                Constants.TITLE_SENDVID -> 6
-                Constants.TITLE_STREAMTAPE -> 7
+                Constants.TITLE_FEMBED -> 2
+                Constants.TITLE_STREAMSB -> 3
+                Constants.TITLE_SENDVID -> 4
+                Constants.TITLE_VIDEOBIN -> 5
+                Constants.TITLE_VOE -> 6
+                Constants.TITLE_OKRU -> 7
                 Constants.TITLE_ZIPPYSHARE -> 8
-                Constants.TITLE_PUJ -> 9
-                Constants.TITLE_EMBED -> 10
-                Constants.TITLE_VIDEOBIN -> 11
-                Constants.TITLE_MP4UPLOAD -> 12
-                Constants.TITLE_UQLOAD -> 13
-                Constants.TITLE_MEGA -> 14
+                Constants.TITLE_BAYFILES -> 9
+                Constants.TITLE_STREAMTAPE -> 10
+                Constants.TITLE_SOLIDFILES -> 11
+                Constants.TITLE_PUJ -> 12
+                Constants.TITLE_GOOGLE_DRIVE -> 13
+                Constants.TITLE_ANONFILE -> 14
+                Constants.TITLE_FIRELOAD -> 15
+                Constants.TITLE_MP4UPLOAD -> 16
+                Constants.TITLE_UQLOAD -> 17
+                Constants.TITLE_MEGA -> 18
                 else -> 10000
             }
         }
