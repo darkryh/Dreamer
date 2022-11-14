5664e3aa5d968326e2c94cbca4a0cafe91f5f0fa
xavier <alex_torres-xc@hotmail.com>
2022-11-14 01:42:11 -0500

    unused

--- app/src/main/java/com/ead/project/dreamer/data/database/model/Player.kt
+++
@@ -1,7 +0,0 @@
-package com.ead.project.dreamer.data.database.model
-
-enum class Player {
-    Bayfiles,StreamSb,Fembed,Fireload,Mega,MegaUp,Mp4Upload,
-    Okru,Onefichier,Puj,Senvid,SolidFiles,Streamtape,
-    Uqload,Videobin,Zippyshare,Voe,Anonfile,Blank
-}
--- app/src/main/java/com/ead/project/dreamer/data/database/model/server/NullServer.kt
+++
@@ -1,5 +0,0 @@
-package com.ead.project.dreamer.data.database.model.server
-
-import com.ead.project.dreamer.data.database.model.Server
-
-class NullServer (embeddedUrl:String) : Server(embeddedUrl)
