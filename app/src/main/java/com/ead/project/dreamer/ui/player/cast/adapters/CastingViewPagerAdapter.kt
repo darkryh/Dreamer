b6b486f52c59ba6bf78dc5726a9501f554823f03
xavier <alex_torres-xc@hotmail.com>
2022-11-14 01:51:17 -0500

    update pager

--- app/src/main/java/com/ead/project/dreamer/ui/player/cast/adapters/CastingViewPagerAdapter.kt
+++ app/src/main/java/com/ead/project/dreamer/ui/player/cast/adapters/CastingViewPagerAdapter.kt
@@ -4,24 +4,20 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ead.project.dreamer.data.database.model.Chapter
-import com.ead.project.dreamer.ui.player.cast.CastingChapterFragment
-import com.ead.project.dreamer.ui.player.cast.CastingChapterListFragment
+import com.ead.project.dreamer.ui.player.cast.CastingChaptersFragment
 import javax.annotation.Nonnull
 
 class CastingViewPagerAdapter (@Nonnull fragmentActivity: FragmentActivity,
                                private val chapterSender: Chapter) : FragmentStateAdapter(fragmentActivity) {
 
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 1
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> CastingChapterFragment().apply {
+            0 -> CastingChaptersFragment().apply {
                 this.chapter = chapterSender
             }
-            1 -> CastingChapterListFragment().apply {
-                this.chapter = chapterSender
-            }
-            else -> CastingChapterFragment().apply{
+            else -> CastingChaptersFragment().apply {
                 this.chapter = chapterSender
             }
         }
