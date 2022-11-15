593f1406c86d53782c931250150202d6536aa259
xavier <alex_torres-xc@hotmail.com>
2022-11-15 07:47:13 -0500

    player enum for Server.java

---
+++ app/src/main/java/com/ead/project/dreamer/data/models/Player.kt
@@ -0,0 +1,7 @@
+package com.ead.project.dreamer.data.models
+
+enum class Player {
+    Bayfiles,StreamSb,Fembed,Fireload,Mega,MegaUp,Mp4Upload,
+    Okru,Onefichier,Puj,Senvid,SolidFiles,Streamtape,Uptobox,
+    Uqload,Videobin,Zippyshare,Voe,Anonfile,DoodStream,Vidlox,YourUpload,Blank
+}
