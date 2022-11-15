6037229f6324ee61232a10bac61df2af5532dfd3
xavier <alex_torres-xc@hotmail.com>
2022-11-15 07:26:47 -0500

    update DownloadManager.kt to work with provider data, in web-view.

--- app/src/main/java/com/ead/project/dreamer/data/utils/DownloadManager.kt
+++ app/src/main/java/com/ead/project/dreamer/data/utils/DownloadManager.kt
@@ -3,71 +3,112 @@ package com.ead.project.dreamer.data.utils
 import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
 import android.content.IntentFilter
-import android.database.Cursor
 import android.graphics.Bitmap
-import android.net.Uri
-import android.os.Environment
 import android.webkit.WebView
-import com.ead.project.dreamer.R
 import com.ead.project.dreamer.app.DreamerApp
-import com.ead.project.dreamer.data.commons.Constants
+import com.ead.project.dreamer.data.AnimeRepository
 import com.ead.project.dreamer.data.commons.Tools
-import com.ead.project.dreamer.data.commons.Tools.Companion.manageFolder
+import com.ead.project.dreamer.data.commons.Tools.Companion.isChapterNotInProgress
+import com.ead.project.dreamer.data.commons.Tools.Companion.load
+import com.ead.project.dreamer.data.commons.Tools.Companion.notContains
+import com.ead.project.dreamer.data.commons.Tools.Companion.onDestroy
 import com.ead.project.dreamer.data.database.model.Chapter
-import com.ead.project.dreamer.data.database.model.Server
-import com.ead.project.dreamer.data.database.model.VideoChecker
+import com.ead.project.dreamer.data.models.Server
+import com.ead.project.dreamer.data.models.VideoChecker
 import com.ead.project.dreamer.data.network.DreamerClient
 import com.ead.project.dreamer.data.network.DreamerWebView
 import com.ead.project.dreamer.data.network.DreamerWebView.Companion.getServerScript
+import com.ead.project.dreamer.data.utils.receiver.DownloadsReceiver
 import kotlinx.coroutines.*
-import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import javax.inject.Inject
 
-open class DownloadManager(
+
+open class DownloadManager @Inject constructor(
     private val context: Context,
-    val chapterList: MutableList<Chapter>) {
+    private val downloadManager: DownloadManager,
+    private val repository: AnimeRepository
+) {
 
+    private var chapterList: MutableList<Chapter> = mutableListOf()
     private var webView : DreamerWebView?= null
-    private var downloadManager : DownloadManager?=null
-    private var idAdapter =  0L
+    private var idDownload =  0L
     private var chapter : Chapter?= null
     private var broadcastReceiver : BroadcastReceiver?= null
-
+    private var executor: ExecutorService = Executors.newSingleThreadExecutor()
     private val scope = CoroutineScope(Dispatchers.IO)
+    private var downloadList : List<Int> = arrayListOf()
 
-    init { settingDownload() }
+    fun init(dataList: List<Chapter>, isFilteredStatus : Boolean = true) {
+        val filteredList = if (isFilteredStatus) filterData(dataList) else filterData(filterStatus(dataList))
+        if (filteredList.isNotEmpty()) {
+            chapterList.addAll(filteredList)
+            runOnUI {
+                DreamerApp.showShortToast("Empezando Descarga.")
+                if (webView == null) settingDownload()
+            }
+        } else runOnUI { DreamerApp.showShortToast("Todos los capítulos descargados.") }
+    }
+
+    fun init(chapter: Chapter) {
+        when (chapter.downloadState) {
+            Chapter.STATUS_INITIALIZED -> {
+                if (isChapterNotInProgress(chapter)) addChapterDownload(chapter,"Empezando descarga.")
+                else runOnUI { DreamerApp.showShortToast("Capítulo descarga en progreso.") }
+            }
+            Chapter.STATUS_RUNNING -> { runOnUI { DreamerApp.showShortToast("Capítulo descarga en progreso.") } }
+            Chapter.STATUS_PENDING -> { runOnUI { DreamerApp.showShortToast("Capítulo descarga pendiente.") } }
+            Chapter.STATUS_PAUSED -> { runOnUI { DreamerApp.showShortToast("Capítulo descarga en pausa.") } }
+            Chapter.STATUS_FAILED -> {
+                val data = Chapter.getDownloadList().singleOrNull{ it.second == chapter.id }
+                data?.let {
+                    downloadManager.remove(it.first)
+                    Chapter.removeFromDownloadList(it)
+                }
+                addChapterDownload(chapter,"Capítulo descarga fallida, Reitendando descarga.")
+            }
+            Chapter.STATUS_COMPLETED -> { runOnUI { DreamerApp.showShortToast("Capítulo descarga completa.") } }
+        }
+    }
+
+    private fun filterData(dataList: List<Chapter>) : List<Chapter> {
+        val temporalList : MutableList<Chapter> = mutableListOf()
+        downloadList = Chapter.getDownloadList().map { it.second }
+        for (data in dataList) if (isChapterNotInProgress(data)) temporalList.add(data)
+        return temporalList
+    }
+
+    private fun filterStatus(dataList: List<Chapter>) = dataList.filter {
+        it.downloadState == Chapter.STATUS_INITIALIZED || it.downloadState == Chapter.STATUS_FAILED }
 
     private fun settingDownload() {
-        downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         webView = DreamerWebView(DreamerApp.INSTANCE)
         webView?.webViewClient = object : DreamerClient() {
-            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                super.onPageStarted(view, url, favicon)
-                run {
-                    if (timeout && url != Constants.BLANK_BROWSER) {
-                        webView?.loadUrl(Constants.BLANK_BROWSER)
-                        DreamerApp.showLongToast(DreamerApp.INSTANCE.getString(R.string.timeout_message))
-                    }
+            override fun onTimeout(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onTimeout(view, url, favicon)
+                chapter?.let {
+                    it.downloadState = Chapter.STATUS_FAILED
+                    executor.execute { repository.updateChapterNormal(it) }
                 }
+                runNextChapter()
             }
 
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                timeout = false
+            override fun onPageLoaded(view: WebView?, url: String?) {
+                super.onPageLoaded(view, url)
                 try {
-                    if (url != Constants.BLANK_BROWSER) {
-                        webView?.evaluateJavascript(getServerScript()) {
-                            val list : MutableList<String> = mutableListOf()
-                            for (string in VideoChecker
-                                .getSorterServerList(Tools.stringRawArrayToList(it),true)) {
-                                list.add(Tools.embedLink(string))
-                            }
-                            getData(list)
+                    webView?.evaluateJavascript(getServerScript()) {
+                        val list : MutableList<String> = mutableListOf()
+                        for (string in VideoChecker
+                            .getSorterServerList(Tools.stringRawArrayToList(it),true)) {
+                            list.add(Tools.embedLink(string))
                         }
+                        getData(list)
                     }
                 } catch (e : Exception) { e.printStackTrace() }
             }
+
         }
         startDownload()
     }
@@ -75,47 +116,11 @@ open class DownloadManager(
     private fun startDownload() {
         chapter = getChapterFromList()
         if (broadcastReceiver == null) {
-            broadcastReceiver = object : BroadcastReceiver() {
-                override fun onReceive(context: Context?, intent: Intent?) {
-                    val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID,-1)
-                    val query: DownloadManager.Query = DownloadManager.Query()
-                    query.setFilterById(id!!)
-                    val cursor: Cursor = downloadManager!!.query(query)
-                    if (cursor.moveToFirst()) {
-                        val columnIndex =
-                            cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
-                        val status = cursor.getInt(columnIndex)
-                        val columnReason =
-                            cursor.getColumnIndex(DownloadManager.COLUMN_REASON)
-                        val reason = cursor.getInt(columnReason)
-                        when(status) {
-                            DownloadManager.STATUS_SUCCESSFUL -> {
-                                DreamerApp.showLongToast("SUCCESSFUL")
-                            }
-                            DownloadManager.STATUS_PAUSED -> {
-                                DreamerApp.showLongToast("PAUSED")
-                            }
-                            DownloadManager.STATUS_RUNNING -> {
-                                DreamerApp.showLongToast("RUNNING")
-                            }
-                            DownloadManager.STATUS_PENDING -> {
-                                DreamerApp.showLongToast("PENDING")
-                            }
-                            DownloadManager.STATUS_FAILED -> {
-                                DreamerApp.showLongToast("FAILED")
-                            }
-                        }
-                    }
-                }
-
-            }
-            context.registerReceiver(broadcastReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+            broadcastReceiver = DownloadsReceiver()
+            context.registerReceiver(broadcastReceiver,
+                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         }
-
-        if (chapter != null) {
-            webView?.loadUrl(chapterList.first().reference)
-        }
-        else onCompleted()
+        webView?.load(chapterList.first().reference)
     }
 
     private fun getChapterFromList() : Chapter? = try { chapterList.first() }
@@ -124,6 +129,16 @@ open class DownloadManager(
     private fun getData(it: List<String>) {
         scope.launch (Dispatchers.IO) {
             prepareDownload(withContext(Dispatchers.Default) { ServerManager.getData(it) })
+        }
+    }
+
+    private fun addChapterDownload(chapter: Chapter,warningMessage : String) {
+        if (isChapterNotInProgress(chapter)) {
+            chapterList.add(chapter)
+            runOnUI {
+                DreamerApp.showShortToast(warningMessage)
+                if (webView == null) settingDownload()
+            }
         }
     }
 
@@ -148,43 +163,43 @@ open class DownloadManager(
     }
 
     private fun handleDownloads(selectedUrl : String) {
-        ThreadUtil.runOnUiThread {
-            val request = DownloadManager.Request(Uri.parse(selectedUrl))
-            val fileDirectory = File(DirectoryManager.getSeriesFolder().absolutePath
-                ,chapter?.title?:"errorFolder")
-            fileDirectory.manageFolder()
-            request.apply {
-                setTitle("${chapter?.title} Cap. ${chapter?.chapterNumber}")
-                setDescription("Capítulo ${chapter?.chapterNumber} Descargando..")
-                setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-                setDestinationInExternalPublicDir(
-                    Environment.DIRECTORY_DOWNLOADS,
-                    DirectoryManager.mainFolder
-                            + "/"  + DirectoryManager.seriesFolder
-                            + "/" + chapter?.title
-                            + "/" + chapter?.title + " Capítulo ${chapter?.chapterNumber}" +".mp4")
+        runOnUI {
+            chapter?.apply {
+                val request = Tools.downloadRequest(this,selectedUrl)
+                idDownload = downloadManager.enqueue(request)
+                Chapter.addToDownloadList(Pair(idDownload,this.id))
+                runNextChapter()
             }
-            downloadManager?.let { idAdapter = it.enqueue(request) }
-            runNextChapter()
         }
     }
 
+    fun isDataNotInProgress(chapter: Chapter) : Boolean {
+        downloadList = Chapter.getDownloadList().map { it.second }
+        return  isChapterNotInProgress(chapter)
+    }
+
+    private fun isChapterNotInProgress(chapter: Chapter) = chapterList.notContains(chapter)
+            && downloadList.notContains(chapter.id)
+            && downloadManager.isChapterNotInProgress(chapter)
+
     private fun runNextChapter() {
-        if (chapter != null) {
-            chapterList.removeFirst()
-            startDownload()
+        runOnUI {
+            if (chapterList.isNotEmpty()) chapterList.removeFirst()
+            if (getChapterFromList() != null) startDownload()
+            else onCompleted()
         }
-        else onCompleted()
     }
 
     open fun onCompleted() {
-        downloadManager = null
-        context.unregisterReceiver(broadcastReceiver)
+        try { context.unregisterReceiver(broadcastReceiver) } catch (e : IllegalArgumentException) { e.printStackTrace()}
         onDestroy()
     }
 
+    private fun runOnUI(task: () -> Unit) = ThreadUtil.onUi { task() }
+
     private fun onDestroy() {
         scope.cancel()
+        webView?.onDestroy()
         webView = null
     }
 }
