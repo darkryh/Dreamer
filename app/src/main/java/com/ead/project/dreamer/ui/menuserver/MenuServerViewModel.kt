b873250a5b60f3682f9915457a97ffa410cade88
xavier <alex_torres-xc@hotmail.com>
2022-11-15 07:44:24 -0500

    formatting code.

--- app/src/main/java/com/ead/project/dreamer/ui/menuserver/MenuServerViewModel.kt
+++ app/src/main/java/com/ead/project/dreamer/ui/menuserver/MenuServerViewModel.kt
@@ -1,15 +1,15 @@
-package com.ead.project.dreamer.ui.menuplayer
+package com.ead.project.dreamer.ui.menuserver
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ead.project.dreamer.data.database.model.Server
+import com.ead.project.dreamer.data.models.Server
 import com.ead.project.dreamer.data.utils.ServerManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MenuPlayerViewModel @Inject constructor () : ViewModel() {
+class MenuServerViewModel @Inject constructor () : ViewModel() {
 
     private var serverList : MutableLiveData<List<Server>> = MutableLiveData()
 
