d0ade18e923a925fdd044005d8a1f3134b49e398
xavier <alex_torres-xc@hotmail.com>
2022-11-15 07:32:22 -0500

    formatting code.

--- app/src/main/java/com/ead/project/dreamer/data/utils/DreamerDiffUtil.kt
+++ app/src/main/java/com/ead/project/dreamer/data/utils/DreamerDiffUtil.kt
@@ -6,6 +6,7 @@ class DreamerDiffUtil <T>(
     private val oldList: List<T>,
     private val newList: List<T>
 ) : DiffUtil.Callback() {
+
     override fun getOldListSize() = oldList.size
 
     override fun getNewListSize() = newList.size
@@ -15,11 +16,8 @@ class DreamerDiffUtil <T>(
             return if (oldList[oldItemPosition] is DiffUtilEquality) {
                 (oldList[oldItemPosition] as DiffUtilEquality)
                     .equalsHeader((newList[oldItemPosition] as DiffUtilEquality))
-            } else
-                true
-        } catch (e : Exception) {
-            false
-        }
+            } else true
+        } catch (e : Exception) { false }
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
@@ -27,10 +25,7 @@ class DreamerDiffUtil <T>(
             return if (oldList[oldItemPosition] is DiffUtilEquality) {
                 (oldList[oldItemPosition] as DiffUtilEquality)
                     .equalsContent((newList[oldItemPosition] as DiffUtilEquality))
-            } else
-                true
-        } catch (e : Exception) {
-            false
-        }
+            } else true
+        } catch (e : Exception) { false }
     }
 }
