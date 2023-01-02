b9c4410c55f3ba360d8813d0497473dbbe5a775c
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:56:49 -0500

    Notification receiver thar manage operations when the user click in notifications actions.

--- app/src/main/java/com/ead/project/dreamer/data/utils/receiver/NotificationReceiver.kt
+++ app/src/main/java/com/ead/project/dreamer/data/utils/receiver/NotificationReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import com.ead.project.dreamer.data.commons.Constants
 import com.ead.project.dreamer.data.utils.DataStore
+import com.ead.project.dreamer.data.utils.NotificationManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -12,33 +13,38 @@ import kotlinx.coroutines.launch
 
 class NotificationReceiver : BroadcastReceiver() {
 
-    companion object {
-        const val PREFERENCE_DEACTIVATION = "PREFERENCE_DEACTIVATION"
-        const val PREFERENCE_ACTIVATION_FAVORITES = "PREFERENCE_ACTIVATION_FAVORITES"
-        const val PREFERENCE_ACTIVATION_ALL = "PREFERENCE_ACTIVATION_ALL"
-        const val NOTIFICATION_ID = "123"
-    }
-
     private val scope = CoroutineScope(SupervisorJob())
-    private val notifier = DreamerNotifier().notificationManager()
+    private var notifier : NotificationManager?=null
 
     override fun onReceive(context: Context, intent: Intent?) {
         val pendingResult: PendingResult = goAsync()
         scope.launch(Dispatchers.Default) {
             try {
                 val id = intent?.getIntExtra(NOTIFICATION_ID, -1) ?: -1
+
                 when(intent?.action) {
                     PREFERENCE_DEACTIVATION -> DataStore.writeIntAsync(Constants.PREFERENCE_NOTIFICATIONS,0)
                     PREFERENCE_ACTIVATION_FAVORITES -> DataStore.writeIntAsync(Constants.PREFERENCE_NOTIFICATIONS,1)
                     PREFERENCE_ACTIVATION_ALL -> DataStore.writeIntAsync(Constants.PREFERENCE_NOTIFICATIONS,2)
+                    PREFERENCE_CHAPTER_WATCH -> {}
+                    PREFERENCE_CHAPTER_INTERESTED -> {}
+                    PREFERENCE_CHAPTER_NOT_INTERESTED -> {}
                 }
-                notifier.cancel(id)
-            } finally {
-                // Must call finish() so the BroadcastReceiver can be recycled
-                pendingResult.finish()
-            }
+                getNotifier(context).cancel(id)
+            } finally { pendingResult.finish() /* Must call finish() so the BroadcastReceiver can be recycled */ }
         }
-
     }
 
+    private fun getNotifier(context: Context) : NotificationManager
+    = notifier?: NotificationManager(context).also { notifier = it }
+
+    companion object {
+        const val PREFERENCE_DEACTIVATION = "PREFERENCE_DEACTIVATION"
+        const val PREFERENCE_ACTIVATION_FAVORITES = "PREFERENCE_ACTIVATION_FAVORITES"
+        const val PREFERENCE_ACTIVATION_ALL = "PREFERENCE_ACTIVATION_ALL"
+        const val PREFERENCE_CHAPTER_WATCH = "PREFERENCE_CHAPTER_WATCH"
+        const val PREFERENCE_CHAPTER_INTERESTED = "PREFERENCE_CHAPTER_INTERESTED"
+        const val PREFERENCE_CHAPTER_NOT_INTERESTED = "PREFERENCE_CHAPTER_NOT_INTERESTED"
+        const val NOTIFICATION_ID = "123"
+    }
 }
