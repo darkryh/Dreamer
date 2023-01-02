bcf965f453473839d8ea212c483f79404620a100
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:38:23 -0500

    query to get all profiles.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetProfileList.kt
@@ -0,0 +1,12 @@
+package com.ead.project.dreamer.domain.databasequeries
+
+import com.ead.project.dreamer.data.AnimeRepository
+import com.ead.project.dreamer.data.database.model.AnimeProfile
+import javax.inject.Inject
+
+class GetProfileList @Inject constructor(
+    private val repository: AnimeRepository
+) {
+
+    suspend operator fun invoke() : List<AnimeProfile> = repository.getProfileList()
+}
