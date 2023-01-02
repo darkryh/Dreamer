52911096ab23b30be0d8f6d666f05dcd115eb9cf
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:33:24 -0500

    get servers from provider chapter webpage.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/GetEmbedServers.kt
@@ -0,0 +1,27 @@
+package com.ead.project.dreamer.domain.servers
+
+import android.content.Context
+import com.ead.project.dreamer.data.database.model.Chapter
+import javax.inject.Inject
+
+class GetEmbedServers @Inject constructor(
+    private val context: Context,
+    private val getServerResultToArray: GetServerResultToArray,
+) {
+
+    private lateinit var chapter: Chapter
+
+    operator fun invoke(timeoutTask : () -> Unit,chapter: Chapter)  {
+        this.chapter = chapter
+        serverEngine(timeoutTask, chapter)
+    }
+
+    private val serverEngine = object  : ServerEngine(context,getServerResultToArray) {
+        override fun getServerList(it: String): List<String> {
+            val result = super.getServerList(it)
+            return result
+        }
+    }
+
+    fun onDestroy() = serverEngine.onDestroy()
+}
