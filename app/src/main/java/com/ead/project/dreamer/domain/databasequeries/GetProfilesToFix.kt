2f4d4c0163cc5d6d4d685d5f09ba3aa857067d67
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:39:39 -0500

    query to get profile release that not have data.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetProfilesToFix.kt
@@ -0,0 +1,14 @@
+package com.ead.project.dreamer.domain.databasequeries
+
+import com.ead.project.dreamer.data.AnimeRepository
+import com.ead.project.dreamer.data.database.model.AnimeProfile
+import javax.inject.Inject
+
+class GetProfilesToFix @Inject constructor(
+    private val repository: AnimeRepository
+) {
+
+    suspend operator fun invoke () : List<AnimeProfile> = repository.getProfilesToFix().filter {
+        it.title != "Jantama Pong☆"
+    }
+}
