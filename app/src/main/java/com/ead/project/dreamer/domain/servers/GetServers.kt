5743aa8c54366c4f130d4a6d14ed24fa1649e217
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:41:42 -0500

    To get multiple servers based in manual or automatic setting.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/GetServers.kt
@@ -0,0 +1,28 @@
+package com.ead.project.dreamer.domain.servers
+
+import com.ead.project.dreamer.data.models.Server
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetServers @Inject constructor(
+    private val getServer: GetServer
+) {
+
+    private val servers : MutableList<Server> = mutableListOf()
+
+    operator fun invoke(serverUrls : List<String>) : List<Server> {
+        servers.clear()
+        for (url in serverUrls) {
+            servers.add(getServer(url))
+            if (Server.isOperationBreak()) {
+                Server.endOperation()
+                break
+            }
+        }
+        return servers
+    }
+
+    suspend fun fromCoroutine(serverUrls: List<String>) : List<Server> =
+        withContext(Dispatchers.IO) { invoke(serverUrls) }
+}
