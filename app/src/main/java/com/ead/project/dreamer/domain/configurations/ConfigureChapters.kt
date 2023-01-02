a058cb772f5d3461b9a1d0088b6af89a316aa782
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:06:50 -0500

    configure chapters for caching, needed chapters.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/configurations/ConfigureChapters.kt
@@ -0,0 +1,48 @@
+package com.ead.project.dreamer.domain.configurations
+
+import androidx.work.*
+import com.ead.project.dreamer.data.AnimeRepository
+import com.ead.project.dreamer.data.commons.Constants
+import com.ead.project.dreamer.data.database.model.AnimeProfile
+import javax.inject.Inject
+
+class ConfigureChapters @Inject constructor(
+    private val repository: AnimeRepository,
+    private val launchOneTimeRequest: LaunchOneTimeRequest,
+) {
+
+    suspend operator fun invoke (id : Int,reference: String) {
+        val dataList : List<Int> = repository.getPreparationProfile(id)
+        val animeProfile : AnimeProfile = repository.getAnimeProfile(id)!!
+        if (dataList.size >= 2) {
+            animeProfile.size = dataList[0]
+            animeProfile.lastChapterId = dataList[1]
+            animeProfile.reference = reference
+        }
+        if (cachingChaptersTrigger(animeProfile)) cachingChapters(
+            animeProfile.id,
+            animeProfile.reference?:"null",
+            animeProfile.size - dataList[0],
+            animeProfile.lastChapterId
+        )
+        repository.updateAnimeProfile(animeProfile)
+    }
+
+    private fun cachingChaptersTrigger (animeProfile: AnimeProfile) =
+        animeProfile.lastChapterId == 0 || animeProfile.state != Constants.PROFILE_FINAL_STATE
+
+    private fun cachingChapters(id : Int, reference : String, size : Int, lastChapterId : Int) {
+        val array = arrayOf(id.toString(),size.toString(),reference,lastChapterId.toString())
+
+        val data = Data.Builder()
+            .putStringArray(Constants.CHAPTER_PROFILE_KEY,array)
+            .build()
+
+        launchOneTimeRequest(
+            LaunchOneTimeRequest.ChaptersCachingWorkerCode,
+            Constants.SYNC_CHAPTER_SIZE,
+            ExistingWorkPolicy.KEEP,
+            data
+        )
+    }
+}
