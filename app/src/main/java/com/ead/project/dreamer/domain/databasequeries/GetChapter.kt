c8e6e9e9bb5d660cf32409c9f52f542e0529829a
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:22:32 -0500

    query referred to chapter.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetChapter.kt
@@ -0,0 +1,18 @@
+package com.ead.project.dreamer.domain.databasequeries
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import com.ead.project.dreamer.data.AnimeRepository
+import com.ead.project.dreamer.data.database.model.Chapter
+import javax.inject.Inject
+
+class GetChapter @Inject constructor(
+    private val repository: AnimeRepository
+) {
+
+    suspend fun fromId(id : Int) : Chapter? =
+        repository.getChapterFromId(id)
+
+    fun livedata (chapter: Chapter) : LiveData<Chapter?> =
+        repository.getFlowChapterFromTitleAndNumber(chapter.title,chapter.number).asLiveData()
+}
