709cb97ef41f278407dbe6e81021970f44bc8ccd
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:33:38 -0500

    get servers from provider chapter webpage, returning mutable.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/GetEmbedServersMutable.kt
@@ -0,0 +1,30 @@
+package com.ead.project.dreamer.domain.servers
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ead.project.dreamer.data.database.model.Chapter
+import javax.inject.Inject
+
+class GetEmbedServersMutable @Inject constructor(
+    private val context: Context,
+    private val getServerResultToArray: GetServerResultToArray,
+) {
+    private lateinit var embedServers : MutableLiveData<List<String>>
+
+    operator fun invoke(timeoutTask : () -> Unit,chapter: Chapter) : LiveData<List<String>> {
+        embedServers = MutableLiveData()
+        serverEngine(timeoutTask, chapter)
+        return embedServers
+    }
+
+    private val serverEngine = object  : ServerEngine(context,getServerResultToArray) {
+        override fun getServerList(it: String): List<String> {
+            val result = super.getServerList(it)
+            embedServers.postValue(result)
+            return result
+        }
+    }
+
+    fun onDestroy() = serverEngine.onDestroy()
+}
