950bc7f8c9784eb54f9cb43d1f1b8db98ea07f08
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 21:57:37 -0500

    updated methods to work with domain download manager.

--- app/src/main/java/com/ead/project/dreamer/ui/chapter/settings/ChapterSettingsFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/ui/chapter/settings/ChapterSettingsFragment.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.ead.commons.lib.lifecycle.parcelable
+import com.ead.commons.lib.views.addSelectableItemEffect
+import com.ead.commons.lib.views.setVisibility
 import com.ead.project.dreamer.R
-import com.ead.project.dreamer.app.DreamerApp
 import com.ead.project.dreamer.data.commons.Constants
-import com.ead.project.dreamer.data.commons.Tools.Companion.parcelable
-import com.ead.project.dreamer.data.commons.Tools.Companion.setVisibility
 import com.ead.project.dreamer.data.database.model.Chapter
-import com.ead.project.dreamer.data.utils.DownloadManager
-import com.ead.project.dreamer.data.utils.ui.DreamerLayout
 import com.ead.project.dreamer.databinding.FragmentChapterSettingsBinding
+import com.ead.project.dreamer.domain.DownloadManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -21,16 +21,20 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class ChapterSettingsFragment : BottomSheetDialogFragment() {
 
-    private lateinit var chapter: Chapter
     @Inject lateinit var downloadManager : DownloadManager
-    @Inject lateinit var mDownloadManager: android.app.DownloadManager
-    private var isCorrectData = false
+    private lateinit var chapterSettingsViewModel: ChapterSettingsViewModel
+    private lateinit var chapter: Chapter
+    private var isChapter = false
+    private var isRecords = false
+    private var downloadStatusReverse =-1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        chapterSettingsViewModel = ViewModelProvider(this)[ChapterSettingsViewModel::class.java]
         arguments?.let {
             chapter = it.parcelable(Constants.REQUESTED_CHAPTER)!!
-            isCorrectData = it.getBoolean(Constants.IS_CORRECT_DATA_FROM_CHAPTER)
+            isChapter = it.getBoolean(Constants.IS_CORRECT_DATA_FROM_CHAPTER_SETTINGS)
+            isRecords = it.getBoolean(Constants.IS_CORRECT_DATA_FROM_RECORDS_SETTINGS)
         }
     }
 
@@ -47,39 +51,90 @@ class ChapterSettingsFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        settingVariables()
+        settingLayouts()
+        settingAutomaticDownload()
+        settingManualDownload()
+        settingRecordsDelete()
+        settingFavorites()
+        settingChapterMode()
+    }
+
+    private fun settingVariables() {
+        downloadStatusReverse =
+            if (chapter.downloadState == Chapter.DOWNLOAD_STATUS_COMPLETED) Chapter.DOWNLOAD_STATUS_INITIALIZED
+            else Chapter.DOWNLOAD_STATUS_COMPLETED
+    }
+
+    private fun settingLayouts() {
         Constants.setDownloadMode(true)
-        DreamerLayout.setClickEffect(binding.lnDownload,requireContext())
-        DreamerLayout.setClickEffect(binding.lnManualDownload,requireContext())
-        DreamerLayout.setClickEffect(binding.lnFavorite,requireContext())
-        binding.lnDownload.setVisibility(isCorrectData)
-        binding.lnDownload.setOnClickListener {
-            downloadManager.init(chapter)
-            dismiss()
+        binding.lnDownload.addSelectableItemEffect()
+        binding.lnManualDownload.addSelectableItemEffect()
+        binding.lnFavorite.addSelectableItemEffect()
+        binding.lnDownload.setVisibility(isChapter)
+        binding.lnChapterMode.setVisibility(isChapter)
+        binding.lnFavorite.setVisibility(isChapter)
+        binding.lnRecordDelete.setVisibility(isRecords)
+        settingDownloadState()
+        settingFavoriteLayout()
+    }
+
+    private fun settingDownloadState() {
+        if (chapter.downloadState == Chapter.DOWNLOAD_STATUS_COMPLETED) {
+            binding.imvChapterMode.setImageResource(R.drawable.ic_assignment_24)
+            binding.txvChapterMode.text = getString(R.string.chapter_mode_to_dont_downloaded)
         }
-        binding.lnManualDownload.setOnClickListener {
-            when(chapter.downloadState) {
-                Chapter.STATUS_INITIALIZED -> {
-                    if (downloadManager.isDataNotInProgress(chapter))
-                        Chapter.launchServer(activity as Context, chapter, true)
-                    else DreamerApp.showShortToast(getString(R.string.warning_chapter_status_in_progress))
-                }
-                Chapter.STATUS_RUNNING -> { DreamerApp.showShortToast(getString(R.string.warning_chapter_status_in_progress)) }
-                Chapter.STATUS_PENDING -> { DreamerApp.showShortToast(getString(R.string.warning_chapter_status_pending)) }
-                Chapter.STATUS_PAUSED -> { DreamerApp.showShortToast(getString(R.string.warning_chapter_status_paused)) }
-                Chapter.STATUS_FAILED -> {
-                    val data = Chapter.getDownloadList().singleOrNull{ it.second == chapter.id }
-                    data?.let {
-                        mDownloadManager.remove(it.first)
-                        Chapter.removeFromDownloadList(it)
-                    }
-                    Chapter.launchServer(activity as Context, chapter, true)
-                }
-                Chapter.STATUS_COMPLETED -> { DreamerApp.showShortToast(getString(R.string.warning_chapter_status_completed)) }
+        else {
+            binding.imvChapterMode.setImageResource(R.drawable.ic_assignment_turned_in_24)
+            binding.txvChapterMode.text = getString(R.string.chapter_mode_to_downloaded)
+        }
+    }
+
+    private fun settingFavoriteLayout() {
+        chapterSettingsViewModel.getProfileIsFavorite(chapter.idProfile)?.let {
+            if (it.isFavorite) {
+                binding.imvFavorites.setImageResource(R.drawable.ic_heart_broken_24)
+                binding.txvFavorites.text = getString(R.string.remove_from_favorites)
             }
+            else {
+                binding.imvFavorites.setImageResource(R.drawable.ic_favorite_24)
+                binding.txvFavorites.text = getString(R.string.add_to_favorites)
+            }
+        }
+    }
+
+    private fun settingAutomaticDownload() {
+        binding.lnDownload.setOnClickListener {
+            downloadManager.startDownload(chapter)
             dismiss()
         }
+    }
+
+    private fun settingManualDownload() {
+        binding.lnManualDownload.setOnClickListener {
+            downloadManager.startManualDownload(chapter, activity as Context)
+            dismiss()
+        }
+    }
+
+    private fun settingRecordsDelete() {
+        binding.lnRecordDelete.setOnClickListener {
+            chapterSettingsViewModel.deleteRecords(chapter.idProfile)
+            dismiss()
+        }
+    }
+
+    private fun settingFavorites() {
         binding.lnFavorite.setOnClickListener {
-            DreamerApp.showLongToast(getString(R.string.in_develop))
+            chapterSettingsViewModel.updateFavoriteProfile(chapter.idProfile)
+            dismiss()
+        }
+    }
+
+    private fun settingChapterMode() {
+        binding.lnChapterMode.setOnClickListener {
+            chapter.downloadState = downloadStatusReverse
+            chapterSettingsViewModel.updateChapter(chapter)
             dismiss()
         }
     }
