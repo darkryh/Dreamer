13cc9d4f906a5cebf2ca162b966bc8338fe35f8b
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 22:14:55 -0500

    Renamed class to work generally downloads.

--- app/src/main/java/com/ead/project/dreamer/data/models/DownloadItem.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/DownloadItem.kt
@@ -3,17 +3,14 @@ package com.ead.project.dreamer.data.models
 import android.app.DownloadManager
 import com.ead.project.dreamer.data.utils.DiffUtilEquality
 
-
-data class ChapterDownload(
-    var idChapter : Int,
-    var idDownload : Long,
-    val idProfile : Int,
+data class DownloadItem(
+    var id : Long,
+    val idReference : Int,
     val title : String,
-    val cover :String,
     val number : Int,
-    var state : Int,
     var current : Int,
-    var total : Int
+    var total : Int,
+    var state : Int
 ) : DiffUtilEquality {
 
     fun isInProgress() =  state != DownloadManager.STATUS_FAILED &&
@@ -22,17 +19,18 @@ data class ChapterDownload(
     override fun equalsHeader(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || javaClass != other.javaClass) return false
-        val chapterDownload: ChapterDownload = other as ChapterDownload
-        return this.idDownload == chapterDownload.idDownload
+        val downloadItem: DownloadItem = other as DownloadItem
+        return this.id == downloadItem.id
     }
 
     override fun equalsContent(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || javaClass != other.javaClass) return false
-        val chapterDownload: ChapterDownload = other as ChapterDownload
-        return this.title == chapterDownload.title
-                && this.state == chapterDownload.state
-                && this.current == chapterDownload.current
-                && this.total == chapterDownload.total
+        val downloadItem: DownloadItem = other as DownloadItem
+        return this.idReference == downloadItem.idReference
+                && this.title == downloadItem.title
+                && this.number == downloadItem.number
+                && this.current == downloadItem.current
+                && this.state == downloadItem.state
     }
 }
