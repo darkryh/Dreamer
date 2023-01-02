c109c7d109c0884102ce762816ba5bc8e025c35a
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-01 23:09:30 -0500

    updated repair with the custom testing that enable when is needed.

--- app/src/main/java/com/ead/project/dreamer/ui/settings/options/SettingsFixerFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/ui/settings/options/SettingsFixerFragment.kt
@@ -1,16 +1,14 @@
 package com.ead.project.dreamer.ui.settings.options
 
-import android.graphics.Color
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.ead.commons.lib.lifecycle.observeOnce
 import com.ead.project.dreamer.R
 import com.ead.project.dreamer.data.commons.Constants
-import com.ead.project.dreamer.data.utils.ui.DreamerLayout
+import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.ui.settings.layouts.DiagnosticViewPreference
 import com.ead.project.dreamer.ui.settings.viewmodels.SettingsFixerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,79 +17,67 @@ import dagger.hilt.android.AndroidEntryPoint
 class SettingsFixerFragment : PreferenceFragmentCompat() {
 
     private val settingsFixerViewModel : SettingsFixerViewModel by viewModels()
-    private lateinit var preferenceStateApi: Preference
-    private lateinit var preferenceProvider: Preference
     private lateinit var preferenceFixer: Preference
+    private lateinit var dvpPreference: DiagnosticViewPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.fixer_preferences, rootKey)
         initLayouts()
     }
     private fun initLayouts() {
-        preferenceStateApi = findPreference(Constants.PREFERENCE_CLICK_DREAMER_CONNECTION)!!
-        preferenceProvider = findPreference(Constants.PREFERENCE_CLICK_MC2_CONNECTION)!!
         preferenceFixer = findPreference(Constants.PREFERENCE_CLICK_FIXER)!!
+        dvpPreference = findPreference(Constants.PREFERENCE_CUSTOMIZED_DIAGNOSTIC_VIEW)!!
+        preferenceFixer.isEnabled = false
+        dvpPreference.setOnTestClickListener {
+            dvpPreference.clearStringBuilder()
+            connecting(
+                Constants.API_APP,
+                getString(R.string.status_try_reconnecting, "la API."),
+                getString(R.string.status_api,"exitosa"),
+                getString(R.string.status_api,"fallida"))
+
+            connecting(
+                Constants.PROVIDER_URL,
+                getString(R.string.status_try_reconnecting, "el Proveedor."),
+                getString(R.string.status_provider,"exitosa"),
+                getString(R.string.status_provider,"fallida"))
+
+            if(!settingsFixerViewModel.isDataFromDatabaseOK()) {
+                dvpPreference.addLog(getString(R.string.status_database_items_incorrect))
+                preferenceFixer.isEnabled = true
+            }
+            else dvpPreference.addLog(getString(R.string.status_database_items_correct))
+
+            settingsFixerViewModel.getEmbedServers({},testChapter).observeOnce(this) {
+                if (it.contains(getString(R.string.null_word))) {
+                    dvpPreference.addLog(getString(R.string.status_server_script_incorrect))
+                    preferenceFixer.isEnabled = true
+                }
+                else dvpPreference.addLog(getString(R.string.status_server_script_correct))
+            }
+        }
+
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        when (preference.key!!) {
-            Constants.PREFERENCE_CLICK_DREAMER_CONNECTION -> {
-                preferenceStateApi.isEnabled = false
-                connecting(
-                    Constants.API_APP,
-                    preferenceStateApi,
-                    getString(R.string.status_api,"exitosa"),
-                    getString(R.string.status_api,"fallida"))
-            }
-            Constants.PREFERENCE_CLICK_MC2_CONNECTION -> {
-                preferenceProvider.isEnabled = false
-                connecting(
-                    Constants.PROVIDER_URL,
-                    preferenceProvider,
-                    getString(R.string.status_provider,"exitosa"),
-                    getString(R.string.status_provider,"fallida"))
-            }
-            Constants.PREFERENCE_CLICK_FIXER -> {
-                preferenceFixer.isEnabled = false
-                settingsFixerViewModel.synchronizeScrapper()
-            }
+        when (preference.key) {
+            Constants.PREFERENCE_CLICK_FIXER -> settingsFixerViewModel.synchronizeScrapper()
         }
         return false
     }
 
-    private fun connecting(url : String,preference: Preference,positiveMessage : String,negativeMessage : String) {
-
+    private fun connecting(url : String, connectingMessage :String, positiveMessage : String, negativeMessage : String) {
         settingsFixerViewModel.getConnectionState(url).observe(viewLifecycleOwner) {
             when(it) {
-                0 -> {
-                    val summary: Spannable = SpannableString(getString(R.string.try_reconnecting))
-                    val color = Color.GRAY
-                    summary.setSpan(ForegroundColorSpan(color), 0, summary.length, 0)
-                    preference.summary = summary
-                    val drawable = DreamerLayout.getDrawable(R.drawable.ic_assignment_24)
-                    DreamerLayout.setColorFilter(drawable,color)
-                    preference.icon = drawable
-                }
-                1 -> {
-                    val summary: Spannable = SpannableString(positiveMessage)
-                    val color = Color.GREEN
-                    summary.setSpan(ForegroundColorSpan(color), 0, summary.length, 0)
-                    preference.summary = summary
-                    val drawable = DreamerLayout.getDrawable(R.drawable.ic_check_24)
-                    DreamerLayout.setColorFilter(drawable,color)
-                    preference.icon = drawable
-                }
+                0 -> dvpPreference.addLog(connectingMessage)
+                1 -> dvpPreference.addLog(positiveMessage)
                 -1 -> {
-                    val summary: Spannable = SpannableString(negativeMessage)
-                    val color = Color.RED
-                    summary.setSpan(ForegroundColorSpan(color), 0, summary.length, 0)
-                    preference.isEnabled = true
-                    preference.summary = summary
-                    val drawable = DreamerLayout.getDrawable(R.drawable.ic_clear)
-                    DreamerLayout.setColorFilter(drawable,color)
-                    preference.icon = drawable
+                    dvpPreference.addLog(negativeMessage)
+                    preferenceFixer.isEnabled = true
                 }
             }
         }
     }
+
+    private val testChapter : Chapter = Chapter(0, 0, "null", "null", -1, "https://monoschinos2.com/ver/cowboy-bebop-latino-episodio-1")
 }
