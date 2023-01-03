b4e81017f4b0b6cdf39194b7b84ef29989e25787
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-03 10:26:08 -0500

    added download item click to update apk and launch chapters.

--- app/src/main/java/com/ead/project/dreamer/ui/download/adapter/DownloadRecyclerViewAdapter.kt
+++ app/src/main/java/com/ead/project/dreamer/ui/download/adapter/DownloadRecyclerViewAdapter.kt
@@ -1,27 +1,37 @@
 package com.ead.project.dreamer.ui.download.adapter
 
+import android.app.Activity
 import android.app.DownloadManager
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
+import com.ead.commons.lib.lifecycle.activity.showShortToast
 import com.ead.commons.lib.views.addSelectableItemEffect
 import com.ead.commons.lib.views.setVisibility
 import com.ead.commons.lib.views.setVisibilityReverse
 import com.ead.project.dreamer.R
+import com.ead.project.dreamer.data.commons.Tools
 import com.ead.project.dreamer.data.commons.Tools.Companion.round
+import com.ead.project.dreamer.data.database.model.Chapter
 import com.ead.project.dreamer.data.models.DownloadItem
+import com.ead.project.dreamer.data.utils.DirectoryManager
 import com.ead.project.dreamer.data.utils.DreamerAsyncDiffUtil
 import com.ead.project.dreamer.databinding.LayoutDownloadBinding
+import com.ead.project.dreamer.domain.ChapterManager
+import kotlinx.coroutines.runBlocking
+import java.io.File
 
 class DownloadRecyclerViewAdapter (
-    private val context: Context
+    private val context: Context,
+    private val chapterManager: ChapterManager
     ) : RecyclerView.Adapter<DownloadRecyclerViewAdapter.ViewHolder>() {
 
     private val downloadManager : DownloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
     private val dreamerAsyncDiffUtil = object : DreamerAsyncDiffUtil<DownloadItem>(){}
     private val differ = AsyncListDiffer(this,dreamerAsyncDiffUtil)
+    private val activity : Activity = context as Activity
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -48,21 +58,43 @@ class DownloadRecyclerViewAdapter (
             binding.txvTitle.text = downloadItem.title
             binding.txvLetter.text = downloadItem.title[0].uppercase()
             binding.txvChapterNumber.text =
-                if (downloadItem.number != -1) context.getString(R.string.chapter_number_short,downloadItem.number.toString())
-                else context.getString(R.string.update)
+                if (downloadItem.number != -1) context.getString(R.string.chapter_number_short,downloadItem.number.toString()) else context.getString(R.string.update)
             val percent = ((downloadItem.current * 100f) / downloadItem.total).round(2).toString()
             binding.txvState.text = context.getString(R.string.current_percent,percent)
             binding.root.addSelectableItemEffect()
             binding.imvCancel.addSelectableItemEffect()
+
             when(downloadItem.state) {
                 DownloadManager.STATUS_SUCCESSFUL -> binding.txvState.setTextColor(context.getColor(R.color.green))
                 DownloadManager.STATUS_FAILED -> binding.txvState.setTextColor(context.getColor(R.color.red))
                 else -> binding.txvState.setTextColor(context.getColor(R.color.blue_light))
             }
-            binding.root.setOnClickListener {}
+
             binding.imvCancel.setVisibility(downloadItem.isInProgress())
             binding.progressBar.setVisibilityReverse(downloadItem.isInProgress())
             binding.imvCancel.setOnClickListener { downloadManager.remove(downloadItem.id) }
+
+            binding.root.setOnClickListener {
+                if (downloadItem.state == DownloadManager.STATUS_SUCCESSFUL) {
+                    when(downloadItem.type) {
+                        DownloadItem.DOWNLOAD_TYPE_CHAPTER -> {
+                            runBlocking {
+                                val chapter : Chapter? =
+                                    chapterManager.getChapter.fromTitleAndNumber(downloadItem.title,downloadItem.number)
+
+                                if (chapter != null) Chapter.manageVideo(context,chapter)
+                                else activity.showShortToast(context.getString(R.string.error_chapter_not_founded))
+                            }
+                        }
+                        DownloadItem.DOWNLOAD_TYPE_UPDATE -> {
+                            val downloadFile : File = DirectoryManager.getVersionFile(downloadItem.title)
+                            if (downloadFile.exists()) Tools.installApk(context,downloadFile)
+                            else activity.showShortToast(context.getString(R.string.error_file_not_founded))
+                        }
+                    }
+                }
+                else activity.showShortToast(context.getString(R.string.warning_chapter_status_in_progress))
+            }
         }
     }
 }
