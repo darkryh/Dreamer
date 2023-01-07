7bc8cb89b234ed3d44ba17d1d54b2a8698a62d58
Darkryh <78747454+Darkryh@users.noreply.github.com>
2023-01-06 21:09:33 -0500

    added method for custom queries, and added checker for the cursor.

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/IsInDownloadManagerProgress.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/IsInDownloadManagerProgress.kt
@@ -1,5 +1,6 @@
 package com.ead.project.dreamer.domain.downloads
 
+import android.database.Cursor
 import com.ead.project.dreamer.data.commons.Tools.Companion.downloadItem
 import com.ead.project.dreamer.data.database.model.Chapter
 import javax.inject.Inject
@@ -10,6 +11,15 @@ class IsInDownloadManagerProgress @Inject constructor(
 
     operator fun invoke(chapter: Chapter) : Boolean {
         val cursor = getCursorFromDownloads()
+        return checkInCursor(chapter, cursor)
+    }
+
+    operator fun invoke(chapter: Chapter,queryId : Long) : Boolean {
+        val cursor = getCursorFromDownloads(queryId)
+        return checkInCursor(chapter, cursor)
+    }
+
+    private fun checkInCursor(chapter: Chapter, cursor: Cursor) : Boolean {
         while (cursor.moveToNext())
             if (chapter.id == cursor.downloadItem().idReference)
                 return true
