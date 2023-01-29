c617ab907e9d8c7f68d2bd563df322ade6c526e7
alex_ <alex_torres-xc@hotmail.com>
2023-01-28 23:57:08 -0500

    convert YourUpload server to WebServer.

--- app/src/main/java/com/ead/project/dreamer/data/models/server/YourUpload.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/server/YourUpload.kt
@@ -2,10 +2,19 @@ package com.ead.project.dreamer.data.models.server
 
 import com.ead.project.dreamer.data.models.Player
 import com.ead.project.dreamer.data.models.Server
+import com.ead.project.dreamer.data.models.VideoModel
+import org.jsoup.Jsoup
 
 class YourUpload(embeddedUrl:String) : Server(embeddedUrl) {
 
     override fun onExtract() {
+        if (isDownloading) return
         player = Player.YourUpload
+        isDirect = false
+        if (!fileDeleted()) addVideo(VideoModel("Default",url))
     }
+
+    private fun fileDeleted() : Boolean = try {
+        Jsoup.connect(url).get().title() == "Content Invalid"
+    } catch (e : Exception) { true }
 }
