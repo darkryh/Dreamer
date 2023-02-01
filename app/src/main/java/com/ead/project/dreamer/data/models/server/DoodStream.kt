0946f2cc8bf6e23f9bc8925d05a8f4f7eb674f17
alex_ <alex_torres-xc@hotmail.com>
2023-01-31 21:08:49 -0500

    Reverted to nothing.

--- app/src/main/java/com/ead/project/dreamer/data/models/server/DoodStream.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/server/DoodStream.kt
@@ -2,14 +2,9 @@ package com.ead.project.dreamer.data.models.server
 
 import com.ead.project.dreamer.data.models.Player
 import com.ead.project.dreamer.data.models.Server
-
 class DoodStream(embeddedUrl:String) : Server(embeddedUrl) {
 
-    //private val token = "needsToken"
-    //regex dsplayer\.hotkeys[^']+'([^']+).+?function
-    //regex makePlay.+?return[^?]+([^"]+)
-
-    override fun onPreExtract() {
+    override fun onExtract() {
         player = Player.DoodStream
     }
 }
