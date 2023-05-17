74f0eadc4b0d98b9b7741b32474595488527f845
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 17:34:47 -0500

    renamed to AppBuild

--- app/src/main/java/com/ead/project/dreamer/app/model/AppStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.ead.project.dreamer.app.model
-
-data class AppStatus(
-    val id: Int,
-    val minVersion: Double,
-    val lastVersion : Double,
-    val resumedVersionNotes : String?=null,
-    val downloadReference : String
-)
