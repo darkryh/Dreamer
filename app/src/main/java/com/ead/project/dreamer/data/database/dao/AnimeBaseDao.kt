d999c28fe8e0dfa8315173e2f513a58b45fee2bd
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 17:33:42 -0500

    formated code

--- app/src/main/java/com/ead/project/dreamer/data/database/dao/AnimeBaseDao.kt
+++ app/src/main/java/com/ead/project/dreamer/data/database/dao/AnimeBaseDao.kt
@@ -1,8 +1,8 @@
 package com.ead.project.dreamer.data.database.dao
 
 import androidx.room.*
-import com.ead.project.dreamer.data.commons.Constants
 import com.ead.project.dreamer.data.database.model.AnimeBase
+import com.ead.project.dreamer.data.database.model.AnimeProfile
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -17,16 +17,28 @@ interface AnimeBaseDao {
     @Query("select * from anime_base_table where id=:id")
     suspend fun getById(id: Int) : AnimeBase
 
-    @Query("select * from anime_base_table where title like '%' || :title || '%' order by title asc")
+    @Query("select * from anime_base_table " +
+            "where title like '%' || :title || '%' order by title asc")
     fun getFlowDataListByName(title : String) : Flow<List<AnimeBase>>
 
-    @Query("select * from anime_base_table where title like '%' || :title || '%' and type not like '%' || '${Constants.TYPE_UNCENSORED}' || '%' order by title asc")
+    @Query("select * from anime_base_table " +
+            "where title like '%' || :title || '%' " +
+            "and type not like '%' || '${AnimeProfile.TYPE_UNCENSORED}' || '%' order by title asc")
     fun getFlowDataListByNameCensured(title : String) : Flow<List<AnimeBase>>
 
-    @Query("select b.id, b.title,b.cover,b.reference,b.type,b.year from (select * from anime_base_table) as b INNER join anime_profile_table as p on b.id = p.id where b.title like '%' || :title || '%' or p.titleAlternate like '%' || :title || '%' order by b.title asc")
+    @Query("select b.id, b.title,b.cover,b.reference,b.type,b.year " +
+            "from (select * from anime_base_table) as b " +
+            "INNER join anime_profile_table as p on b.id = p.id " +
+            "where b.title like '%' || :title || '%' " +
+            "or p.titleAlternate like '%' || :title || '%' order by b.title asc")
     fun getFlowDataFullListByName(title : String) : Flow<List<AnimeBase>>
 
-    @Query("select b.id, b.title,b.cover,b.reference,b.type,b.year from (select * from anime_base_table) as b INNER join anime_profile_table as p on b.id = p.id where b.title like '%' || :title || '%' or p.titleAlternate like '%' || :title || '%' and type not like '%' || '${Constants.TYPE_UNCENSORED}' || '%' order by b.title asc")
+    @Query("select b.id, b.title,b.cover,b.reference,b.type,b.year " +
+            "from (select * from anime_base_table) as b " +
+            "INNER join anime_profile_table as p on b.id = p.id " +
+            "where b.title like '%' || :title || '%' " +
+            "or p.titleAlternate like '%' || :title || '%' " +
+            "and type not like '%' || '${AnimeProfile.TYPE_UNCENSORED}' || '%' order by b.title asc")
     fun getFlowDataFullListByNameCensured(title : String) : Flow<List<AnimeBase>>
 
     @Query("select * from anime_base_table where title=:title")
