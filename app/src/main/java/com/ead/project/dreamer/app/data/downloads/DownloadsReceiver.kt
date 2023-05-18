74935811f8b42a12444b5e17226f7552153f301f
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:12:49 -0500

    renamed for download events and references

--- app/src/main/java/com/ead/project/dreamer/app/data/downloads/DownloadsReceiver.kt
+++ app/src/main/java/com/ead/project/dreamer/app/data/downloads/DownloadsReceiver.kt
@@ -1,4 +1,4 @@
-package com.ead.project.dreamer.data.utils.receiver
+package com.ead.project.dreamer.app.data.downloads
 
 import android.app.DownloadManager
 import android.content.BroadcastReceiver
@@ -7,72 +7,93 @@ import android.content.Context
 import android.content.Intent
 import android.database.Cursor
 import android.util.Log
+import com.ead.project.dreamer.app.data.player.PlayerPreferences
 import com.ead.project.dreamer.data.AnimeRepository
 import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.data.utils.Run
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ChaptersReceiver : BroadcastReceiver() {
+class DownloadsReceiver : BroadcastReceiver() {
 
     private lateinit var data: Pair<Long,Int>
     private var executor: ExecutorService = Executors.newSingleThreadExecutor()
-    private lateinit var chapter: Chapter
+
     @Inject lateinit var repository: AnimeRepository
     @Inject lateinit var downloadManager : DownloadManager
+    @Inject lateinit var playerPreferences: PlayerPreferences
+
 
     override fun onReceive(context: Context, intent: Intent) {
-        try {
+        Run.catching {
             val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID,-1)
             val query: DownloadManager.Query = DownloadManager.Query()
             query.setFilterById(id)
-            data = Chapter.getDownloadList().single { it.first == id }
-            chapter = runBlocking(Dispatchers.IO) { repository.getChapterFromId(data.second)!! }
+            data = emptyList<Pair<Long,Int>>().single { it.first == id }
+            val chapterToQuery = runBlocking { repository.getChapterFromId(data.second)!! }
 
             val cursor: Cursor = downloadManager.query(query)
 
             if (cursor.moveToFirst()) {
+
                 val columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
                 val status = cursor.getInt(columnIndex)
                 val columnReason = cursor.getColumnIndex(DownloadManager.COLUMN_REASON)
                 val reason = cursor.getInt(columnReason)
-                when(status) {
+
+                val chapterResult : Chapter = when(status) {
                     DownloadManager.STATUS_SUCCESSFUL -> {
-                        chapter.downloadState = Chapter.DOWNLOAD_STATUS_COMPLETED
-                        Chapter.removeFromDownloadList(data)
+                        chapterToQuery.copy(state = Chapter.STATUS_DOWNLOADED)
+                        /*Chapter.removeFromDownloadList(data)*/
                     }
-                    DownloadManager.STATUS_PAUSED -> { chapter.downloadState = Chapter.DOWNLOAD_STATUS_PAUSED }
-                    DownloadManager.STATUS_RUNNING -> { chapter.downloadState = Chapter.DOWNLOAD_STATUS_RUNNING }
-                    DownloadManager.STATUS_PENDING -> { chapter.downloadState = Chapter.DOWNLOAD_STATUS_PENDING }
+                    DownloadManager.STATUS_PAUSED -> { chapterToQuery.copy(state = Chapter.STATUS_PAUSED) }
+                    DownloadManager.STATUS_RUNNING -> { chapterToQuery.copy(state = Chapter.STATUS_RUNNING) }
+                    DownloadManager.STATUS_PENDING -> { chapterToQuery.copy(state = Chapter.STATUS_PENDING) }
                     DownloadManager.STATUS_FAILED -> {
-                        chapter.downloadState = Chapter.DOWNLOAD_STATUS_FAILED
                         downloadManager.remove(id)
-                        Chapter.removeFromDownloadList(data)
+                        /*Chapter.removeFromDownloadList(data)*/
+                        chapterToQuery.copy(state = Chapter.STATUS_FAILED)
                     }
+                    else -> { chapterToQuery }
                 }
+
                 when (reason) {
                     DownloadManager.ERROR_CANNOT_RESUME -> { Log.d(TAG, "ERROR_CANNOT_RESUME") }
+
                     DownloadManager.ERROR_DEVICE_NOT_FOUND -> { Log.d(TAG, "ERROR_DEVICE_NOT_FOUND") }
+
                     DownloadManager.ERROR_FILE_ALREADY_EXISTS -> { Log.d(TAG, "ERROR_FILE_ALREADY_EXISTS") }
+
                     DownloadManager.ERROR_FILE_ERROR -> { Log.d(TAG, "ERROR_FILE_ERROR") }
+
                     DownloadManager.ERROR_HTTP_DATA_ERROR -> { Log.d(TAG, "ERROR_HTTP_DATA_ERROR") }
+
                     DownloadManager.ERROR_INSUFFICIENT_SPACE -> { Log.d(TAG, "ERROR_INSUFFICIENT_SPACE") }
+
                     DownloadManager.ERROR_TOO_MANY_REDIRECTS -> { Log.d(TAG, "ERROR_TOO_MANY_REDIRECTS") }
+
                     DownloadManager.ERROR_UNHANDLED_HTTP_CODE -> { Log.d(TAG, "ERROR_UNHANDLED_HTTP_CODE") }
+
                     DownloadManager.ERROR_UNKNOWN -> { Log.d(TAG, "ERROR_UNKNOWN") }
                 }
+
+                updateCases(chapterResult)
+
             }
-            updateCases()
-        } catch (e : Exception) { e.printStackTrace() }
+        }
     }
 
-    private fun updateCases() {
-        if (chapter.id == (Chapter.get()?.id ?: -1)) Chapter.set(chapter)
-        if (chapter.id == (Chapter.getCasting()?.id?:-1)) Chapter.setCasting(chapter)
+    private fun updateCases(chapter: Chapter) {
+        if (chapter.id == (playerPreferences.getChapter()?.id ?: -1)) {
+            playerPreferences.setChapter(chapter)
+        }
+        if (chapter.id == (playerPreferences.getChapter()?.id?:-1)) {
+            playerPreferences.setCastingChapter(chapter)
+        }
         executor.execute { repository.updateChapterNormal(chapter) }
     }
 }
