3e09c88e6ff1d0d87da784e3ef9aba63656c3799
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:32:51 -0500

    MonosChinos object that have data from the provider

---
+++ app/src/main/java/com/ead/project/dreamer/app/data/monos_chinos/MonosChinos.kt
@@ -0,0 +1,11 @@
+package com.ead.project.dreamer.app.data.monos_chinos
+
+object MonosChinos {
+
+    const val URL = "https://monoschinos2.com/"
+    const val LIST = "animes/"
+    const val PAGE = "animes?p="
+
+    const val CAP_BLANK_MC2 = "https://monoschinos2.com/public/img/capblank.png"
+    const val CAP_BLANK_ANIME_MC2 = "https://monoschinos2.com/public/img/anime.png"
+}
