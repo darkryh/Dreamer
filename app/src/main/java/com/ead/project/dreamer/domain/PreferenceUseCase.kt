e097369e576d8f65799d631b353e15aed32034a7
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:45:46 -0500

    preferences parent class that hold all preferences

---
+++ app/src/main/java/com/ead/project/dreamer/domain/PreferenceUseCase.kt
@@ -0,0 +1,17 @@
+package com.ead.project.dreamer.domain
+
+import com.ead.project.dreamer.app.data.action.ActionStore
+import com.ead.project.dreamer.app.data.ads.AdPreferences
+import com.ead.project.dreamer.app.data.files.FilesPreferences
+import com.ead.project.dreamer.app.data.player.PlayerPreferences
+import com.ead.project.dreamer.app.data.preference.AppBuildPreferences
+import com.ead.project.dreamer.app.data.preference.Preferences
+
+data class PreferenceUseCase(
+    val appBuildPreferences: AppBuildPreferences,
+    val actionStore: ActionStore,
+    val preferences: Preferences,
+    val adPreferences: AdPreferences,
+    val filesPreferences: FilesPreferences,
+    val playerPreferences: PlayerPreferences
+)
