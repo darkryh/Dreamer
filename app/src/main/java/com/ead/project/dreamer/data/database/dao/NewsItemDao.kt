bdd0da210c1c294a04d33a3623dc20772d50975b
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:27:49 -0500

    formatted code

--- app/src/main/java/com/ead/project/dreamer/data/database/dao/NewsItemDao.kt
+++ app/src/main/java/com/ead/project/dreamer/data/database/dao/NewsItemDao.kt
@@ -19,6 +19,15 @@ interface NewsItemDao {
     @Query("select * from anime_news_item_table order by id desc")
     fun getFlowDataList() : Flow<List<NewsItem>>
 
-    @Query("select * from anime_news_item_table where title not like '%hentai%' and title not like '%ecchi%' and title not like '%bragas%' and title not like '%desnudo%'  and title not like '%trasero%' and title not like '%profana%' and title not like '%adulto%' and title not like '%adultos%' and title not like '%pornhub%' and title not like '%sexy%' and title not like '%censura%' and title not like '%bikini%' and title not like '%+18%' and title not like '%sensual%' and title not like '%agua%gamer%' and title not like '%porno%' order by id desc")
+    @Query("select * from anime_news_item_table " +
+            "where title not like '%hentai%' " +
+            "and title not like '%ecchi%' and title not like '%bragas%' " +
+            "and title not like '%desnudo%'  and title not like '%trasero%' " +
+            "and title not like '%profana%' and title not like '%adulto%' " +
+            "and title not like '%adultos%' and title not like '%pornhub%' " +
+            "and title not like '%sexy%' and title not like '%censura%' " +
+            "and title not like '%bikini%' and title not like '%+18%' " +
+            "and title not like '%sensual%' and title not like '%agua%gamer%' " +
+            "and title not like '%porno%' order by id desc")
     fun getFlowDataListCensured() : Flow<List<NewsItem>>
 }
