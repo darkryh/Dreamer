b95b8d703fc6b944fa6673d9d6a1a78e276bb08e
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:24:36 -0500

    added preferences provided

--- app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetDirectoryList.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetDirectoryList.kt
@@ -3,18 +3,21 @@ package com.ead.project.dreamer.domain.databasequeries
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import com.ead.project.dreamer.data.AnimeRepository
-import com.ead.project.dreamer.data.commons.Constants
 import com.ead.project.dreamer.data.database.model.AnimeBase
+import com.ead.project.dreamer.domain.PreferenceUseCase
 import javax.inject.Inject
 
 class GetDirectoryList @Inject constructor(
-    private val repository: AnimeRepository
+    private val repository: AnimeRepository,
+    preferenceUseCase: PreferenceUseCase
 ) {
+
+    private val appBuildPreferences = preferenceUseCase.appBuildPreferences
 
     suspend operator fun invoke() : List<AnimeBase> = repository.getDirectory()
 
     fun livedata (title : String,isSynchronized : Boolean) : LiveData<List<AnimeBase>> =
-        if (Constants.isGooglePolicyNotActivate()) {
+        if (appBuildPreferences.isLockedVersion()) {
             if (isSynchronized) repository.getFlowAnimeBaseFullList(title)
             else repository.getFlowAnimeBaseList(title)
         } else {
