a1f223a7f70a99c0919151721d7041bbf15ad9fb
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 23:39:40 -0500

    added preferences references

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/ConfigureDownloadRequest.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/ConfigureDownloadRequest.kt
@@ -3,25 +3,34 @@ package com.ead.project.dreamer.domain.downloads
 import android.app.DownloadManager
 import android.net.Uri
 import android.os.Environment
-import com.ead.project.dreamer.data.commons.Tools.Companion.manageFolder
-import com.ead.project.dreamer.data.commons.Tools.Companion.toJson
+import com.ead.project.dreamer.app.data.util.system.manageFolder
 import com.ead.project.dreamer.data.database.model.Chapter
-import com.ead.project.dreamer.data.models.DownloadItem
-import com.ead.project.dreamer.data.utils.DirectoryManager
+import com.ead.project.dreamer.data.models.Download
+import com.ead.project.dreamer.domain.PreferenceUseCase
+import com.google.gson.Gson
 import java.io.File
 import javax.inject.Inject
 
-class ConfigureDownloadRequest @Inject constructor() {
+class ConfigureDownloadRequest @Inject constructor(
+    private val gson: Gson,
+    preferenceUseCase: PreferenceUseCase
+) {
+
+    private val filesPreferences = preferenceUseCase.filesPreferences
 
     operator fun invoke (chapter: Chapter, url : String) : DownloadManager.Request {
-        val fileDirectory = File(DirectoryManager.getSeriesFolder().absolutePath, chapter.title)
+        val fileDirectory = File(filesPreferences.series.absolutePath, chapter.title)
         fileDirectory.manageFolder()
         return DownloadManager.Request(Uri.parse(url)).apply {
             setTitle("${chapter.title} Cap.${chapter.number}")
-            setDescription(DownloadItem(0,chapter.id,chapter.title,chapter.number,DownloadItem.DOWNLOAD_TYPE_CHAPTER,0,0,0).toJson())
+            setDescription(
+                gson.toJson(
+                    Download(0,chapter.id,chapter.title,chapter.number,Download.DOWNLOAD_TYPE_CHAPTER,0,0,0)
+                )
+            )
             setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
             setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,
-                DirectoryManager.mainFolder + "/"  + DirectoryManager.seriesFolder + "/" + chapter.title + "/" + chapter.title + " Capítulo ${chapter.number}" + ".mp4"
+                filesPreferences.getChapterRoute(chapter)
             )
         }
     }
@@ -29,7 +38,9 @@ class ConfigureDownloadRequest @Inject constructor() {
     operator fun invoke(title: String, url: String) : DownloadManager.Request {
         return DownloadManager.Request(Uri.parse(url)).apply {
             setTitle(title)
-            setDescription(DownloadItem(0,-1,title,-1,DownloadItem.DOWNLOAD_TYPE_UPDATE,0,0,0).toJson())
+            setDescription(
+                gson.toJson(Download(0,-1,title,-1,Download.DOWNLOAD_TYPE_UPDATE,0,0,0))
+            )
             setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
             setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"$title.apk")
         }
