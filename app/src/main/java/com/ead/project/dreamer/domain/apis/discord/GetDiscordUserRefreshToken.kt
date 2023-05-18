9f8211c954b57a702075f36b3bc7fe54c6e26cdd
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:26:13 -0500

    renamed references

--- app/src/main/java/com/ead/project/dreamer/domain/apis/discord/GetDiscordUserRefreshToken.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/apis/discord/GetDiscordUserRefreshToken.kt
@@ -3,7 +3,7 @@ package com.ead.project.dreamer.domain.apis.discord
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.ead.project.dreamer.data.AnimeRepository
-import com.ead.project.dreamer.data.models.discord.AccessToken
+import com.ead.project.dreamer.data.models.discord.DiscordToken
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,22 +13,22 @@ class GetDiscordUserRefreshToken @Inject constructor(
     private val repository: AnimeRepository
 ) {
 
-    fun livedata () : MutableLiveData<AccessToken?> = getRefreshAccessToken()
+    fun livedata () : MutableLiveData<DiscordToken?> = getRefreshAccessToken()
 
-    private var refreshAccessToken : MutableLiveData<AccessToken?>?= null
+    private var refreshDiscordToken : MutableLiveData<DiscordToken?>?= null
 
-    private fun getRefreshAccessToken() : MutableLiveData<AccessToken?> {
+    private fun getRefreshAccessToken() : MutableLiveData<DiscordToken?> {
         val discordService = repository.getDiscordService(repository.getDiscordUserRefreshTokenRetrofit())
-        val response : Call<AccessToken?> = discordService.getAccessToken()
-        response.enqueue(object : Callback<AccessToken?> {
-            override fun onResponse(call: Call<AccessToken?>, response: Response<AccessToken?>) {
-                try { if (response.isSuccessful) refreshAccessToken?.value = response.body() }
+        val response : Call<DiscordToken?> = discordService.getAccessToken()
+        response.enqueue(object : Callback<DiscordToken?> {
+            override fun onResponse(call: Call<DiscordToken?>, response: Response<DiscordToken?>) {
+                try { if (response.isSuccessful) refreshDiscordToken?.value = response.body() }
                 catch ( e : Exception) { e.printStackTrace() }
             }
-            override fun onFailure(call: Call<AccessToken?>, t: Throwable) {
+            override fun onFailure(call: Call<DiscordToken?>, t: Throwable) {
                 Log.e("error", "onFailure: ${t.cause?.message.toString()}", )
             }
         })
-        return refreshAccessToken?:MutableLiveData<AccessToken?>().also { refreshAccessToken = it }
+        return refreshDiscordToken?:MutableLiveData<DiscordToken?>().also { refreshDiscordToken = it }
     }
 }
--- app/src/main/java/com/ead/project/dreamer/domain/apis/discord/GetDiscordUserToken.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/apis/discord/GetDiscordUserToken.kt
@@ -3,7 +3,7 @@ package com.ead.project.dreamer.domain.apis.discord
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.ead.project.dreamer.data.AnimeRepository
-import com.ead.project.dreamer.data.models.discord.AccessToken
+import com.ead.project.dreamer.data.models.discord.DiscordToken
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,22 +13,22 @@ class GetDiscordUserToken @Inject constructor(
     private val repository: AnimeRepository
 ) {
 
-    fun livedata () : MutableLiveData<AccessToken?> = getAccessToken()
+    fun livedata () : MutableLiveData<DiscordToken?> = getAccessToken()
 
-    private var accessToken : MutableLiveData<AccessToken?>?= null
+    private var discordToken : MutableLiveData<DiscordToken?>?= null
 
-    private fun getAccessToken() : MutableLiveData<AccessToken?> {
+    private fun getAccessToken() : MutableLiveData<DiscordToken?> {
         val discordService = repository.getDiscordService(repository.getDiscordUserTokenRetrofit())
-        val response : Call<AccessToken?> = discordService.getAccessToken()
-        response.enqueue(object : Callback<AccessToken?> {
-            override fun onResponse(call: Call<AccessToken?>, response: Response<AccessToken?>) {
-                try { if (response.isSuccessful) accessToken?.value = response.body() }
+        val response : Call<DiscordToken?> = discordService.getAccessToken()
+        response.enqueue(object : Callback<DiscordToken?> {
+            override fun onResponse(call: Call<DiscordToken?>, response: Response<DiscordToken?>) {
+                try { if (response.isSuccessful) discordToken?.value = response.body() }
                 catch ( e : Exception) { e.printStackTrace() }
             }
-            override fun onFailure(call: Call<AccessToken?>, t: Throwable) {
+            override fun onFailure(call: Call<DiscordToken?>, t: Throwable) {
                 Log.e("error", "onFailure: ${t.cause?.message.toString()}", )
             }
         })
-        return accessToken?:MutableLiveData<AccessToken?>().also { accessToken = it }
+        return discordToken?:MutableLiveData<DiscordToken?>().also { discordToken = it }
     }
 }
