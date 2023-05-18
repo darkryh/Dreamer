2f2661d33e9ff8fb0d67a039aa61ea77be42acde
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:20:56 -0500

    renamed references

--- app/src/main/java/com/ead/project/dreamer/domain/apis/app/GetAppStatusVersion.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/apis/app/GetAppStatusVersion.kt
@@ -2,7 +2,7 @@ package com.ead.project.dreamer.domain.apis.app
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import com.ead.project.dreamer.app.model.AppStatus
+import com.ead.project.dreamer.app.model.AppBuild
 import com.ead.project.dreamer.data.AnimeRepository
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,22 +13,22 @@ class GetAppStatusVersion @Inject constructor(
     private val repository: AnimeRepository
 ) {
 
-    fun livedata () : MutableLiveData<AppStatus> = getAppStatus()
+    fun livedata () : MutableLiveData<AppBuild> = getAppStatus()
 
-    private var appStatus : MutableLiveData<AppStatus>?= null
+    private var appBuild : MutableLiveData<AppBuild>?= null
 
-    private fun getAppStatus() : MutableLiveData<AppStatus> {
+    private fun getAppStatus() : MutableLiveData<AppBuild> {
         val appService = repository.getAppService(repository.getAppRetrofit())
-        val response : Call<AppStatus> = appService.getAppStatus()
-        response.enqueue(object : Callback<AppStatus> {
-            override fun onResponse(call: Call<AppStatus>, response: Response<AppStatus>) {
-                try { if (response.isSuccessful) appStatus?.value = response.body() }
+        val response : Call<AppBuild> = appService.getAppStatus()
+        response.enqueue(object : Callback<AppBuild> {
+            override fun onResponse(call: Call<AppBuild>, response: Response<AppBuild>) {
+                try { if (response.isSuccessful) appBuild?.value = response.body() }
                 catch ( e : Exception) { e.printStackTrace() }
             }
-            override fun onFailure(call: Call<AppStatus>, t: Throwable) {
+            override fun onFailure(call: Call<AppBuild>, t: Throwable) {
                 Log.e("error", "onFailure: ${t.cause?.message.toString()}", )
             }
         })
-        return appStatus?:MutableLiveData<AppStatus>().also { appStatus = it }
+        return appBuild?:MutableLiveData<AppBuild>().also { appBuild = it }
     }
 }
