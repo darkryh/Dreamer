e950b0cff361818cc982a54e5a071d31065361a9
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:15:47 -0500

    updated packages and references

--- app/src/main/java/com/ead/project/dreamer/domain/servers/ServerIdentifier.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/ServerIdentifier.kt
@@ -1,36 +1,37 @@
 package com.ead.project.dreamer.domain.servers
 
-import com.ead.project.dreamer.data.commons.Constants
-import com.ead.project.dreamer.data.commons.Tools.Companion.contains
+import com.ead.project.dreamer.app.data.server.Server
+import com.ead.project.dreamer.app.data.util.system.contains
+
 import javax.inject.Inject
 
 class ServerIdentifier @Inject constructor() {
 
     operator fun invoke(url:String) : String {
-        if (url.contains(Constants.SERVER_OKRU)) return Constants.TITLE_OKRU
-        if (url.contains(Constants.SERVER_SOLIDFILES)) return Constants.TITLE_SOLIDFILES
-        if (url.contains(Constants.SERVER_ONEFICHIER)) return Constants.TITLE_ONEFICHIER
-        if (url.contains(Constants.SERVER_GOOGLE_DRIVE)) return Constants.TITLE_GOOGLE_DRIVE
-        if (url.contains(Constants.SERVER_FEMBED)) return Constants.TITLE_FEMBED
-        if (url.contains(Constants.SERVER_SENDVID)) return Constants.TITLE_SENDVID
-        if (url.contains(Constants.SERVER_DOOD_STREAM)) return Constants.TITLE_DOOD_STREAM
-        if (url.contains(Constants.SERVER_BAYFILES)) return Constants.TITLE_BAYFILES
-        if (url.contains(Constants.SERVER_ZIPPYSHARE)) return Constants.TITLE_ZIPPYSHARE
-        if (url.contains(Constants.SERVER_MEDIAFIRE)) return Constants.TITLE_MEDIAFIRE
-        if (url.contains(Constants.SERVER_FIRELOAD)) return Constants.TITLE_FIRELOAD
-        if (url.contains(Constants.SERVER_STREAMTAPE)) return Constants.TITLE_STREAMTAPE
-        if (url.contains(Constants.SERVER_VOE)) return Constants.TITLE_VOE
-        if (url.contains(Constants.SERVER_PUJ)) return Constants.TITLE_PUJ
-        if (url.contains(Constants.SERVER_VIDEOBIN)) return Constants.TITLE_VIDEOBIN
-        if (url.contains(Constants.SERVER_MP4UPLOAD)) return Constants.TITLE_MP4UPLOAD
-        if (url.contains(Constants.SERVER_UQLOAD)) return Constants.TITLE_UQLOAD
-        if (url.contains(Constants.SERVER_MEGA)) return Constants.TITLE_MEGA
-        if (url.contains(Constants.SERVER_UPTOBOX)) return Constants.TITLE_UPTOBOX
-        if (url.contains(Constants.SERVER_ANONFILE)) return  Constants.TITLE_ANONFILE
-        if (url.contains(Constants.SERVER_YOUR_UPLOAD)) return Constants.TITLE_YOUR_UPLOAD
-        if (url.contains(Constants.SERVER_MEGA_UP)) return Constants.TITLE_MEGA_UP
-        if (url.contains(Constants.SERVER_STREAMSB_DOMAINS)) return Constants.TITLE_STREAMSB
-        if (url.contains(Constants.SERVER_VIDLOX)) return Constants.TITLE_VIDLOX
+        if (url.contains(Server.URL_OKRU)) return Server.OKRU
+        if (url.contains(Server.URL_SOLIDFILES)) return Server.SOLIDFILES
+        if (url.contains(Server.URL_ONEFICHIER)) return Server.ONEFICHIER
+        if (url.contains(Server.URL_GOOGLE_DRIVE)) return Server.GOOGLE_DRIVE
+        if (url.contains(Server.URL_FEMBED)) return Server.FEMBED
+        if (url.contains(Server.URL_SENDVID)) return Server.SENDVID
+        if (url.contains(Server.URL_DOOD_STREAM)) return Server.DOOD_STREAM
+        if (url.contains(Server.URL_BAYFILES)) return Server.BAYFILES
+        if (url.contains(Server.URL_ZIPPYSHARE)) return Server.ZIPPYSHARE
+        if (url.contains(Server.URL_MEDIAFIRE)) return Server.MEDIAFIRE
+        if (url.contains(Server.URL_FIRELOAD)) return Server.FIRELOAD
+        if (url.contains(Server.URL_STREAMTAPE)) return Server.STREAMTAPE
+        if (url.contains(Server.URL_VOE)) return Server.VOE
+        if (url.contains(Server.URL_PUJ)) return Server.PUJ
+        if (url.contains(Server.URL_VIDEOBIN)) return Server.VIDEOBIN
+        if (url.contains(Server.URL_MP4UPLOAD)) return Server.MP4UPLOAD
+        if (url.contains(Server.URL_UQLOAD)) return Server.UQLOAD
+        if (url.contains(Server.URL_MEGA)) return Server.MEGA
+        if (url.contains(Server.URL_UPTOBOX)) return Server.UPTOBOX
+        if (url.contains(Server.URL_ANONFILE)) return  Server.ANONFILE
+        if (url.contains(Server.URL_YOUR_UPLOAD)) return Server.YOUR_UPLOAD
+        if (url.contains(Server.URL_MEGA_UP)) return Server.MEGA_UP
+        if (url.contains(Server.URL_STREAMSB_DOMAINS)) return Server.STREAMSB
+        if (url.contains(Server.URL_VIDLOX)) return Server.VIDLOX
         return "null"
     }
 }
