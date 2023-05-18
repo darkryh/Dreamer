98997e8c4d2934aba4efe49dd178fcd82b824422
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:10:14 -0500

    added preference class that hold scrap state mainly and server script

--- app/src/main/java/com/ead/project/dreamer/data/worker/ScrapperWorker.kt
+++ app/src/main/java/com/ead/project/dreamer/data/worker/ScrapperWorker.kt
@@ -4,9 +4,21 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.ead.project.dreamer.app.model.scrapping.*
-import com.ead.project.dreamer.data.network.DreamerWebView
-import com.ead.project.dreamer.domain.*
+import com.ead.project.dreamer.app.data.server.Server
+import com.ead.project.dreamer.app.model.scraper.AnimeBaseScrap
+import com.ead.project.dreamer.app.model.scraper.AnimeProfileScrap
+import com.ead.project.dreamer.app.model.scraper.ChapterHomeScrap
+import com.ead.project.dreamer.app.model.scraper.ChapterScrap
+import com.ead.project.dreamer.app.model.scraper.NewsItemScrap
+import com.ead.project.dreamer.app.model.scraper.NewsItemWebScrap
+import com.ead.project.dreamer.domain.ChapterUseCase
+import com.ead.project.dreamer.domain.DirectoryUseCase
+import com.ead.project.dreamer.domain.HomeUseCase
+import com.ead.project.dreamer.domain.NewsUseCase
+import com.ead.project.dreamer.domain.PreferenceUseCase
+import com.ead.project.dreamer.domain.ProfileUseCase
+import com.ead.project.dreamer.domain.ServerUseCase
+import com.google.gson.Gson
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
@@ -22,20 +34,22 @@ class ScrapperWorker  @AssistedInject constructor(
     private val directoryUseCase: DirectoryUseCase,
     private val homeUseCase: HomeUseCase,
     private val newsUseCase: NewsUseCase,
-    private val profileUseCase: ProfileUseCase
+    private val profileUseCase: ProfileUseCase,
+    private val gson: Gson,
+    preferenceUseCase: PreferenceUseCase
 ) : CoroutineWorker(context,workerParameters) {
 
+    private val preferences = preferenceUseCase.preferences
     override suspend fun doWork(): Result {
         return withContext(Dispatchers.IO) {
             try {
-
-                DreamerWebView.setServerScript(serverUseCase.getServerScript())
-                AnimeBaseScrap.set(directoryUseCase.getDirectoryScrap.fromApi())
-                AnimeProfileScrap.set(profileUseCase.getProfileScrap.fromApi())
-                ChapterHomeScrap.set(homeUseCase.getHomeScrap.fromApi())
-                ChapterScrap.set(chapterUseCase.getChapterScrap.fromApi())
-                NewsItemScrap.set(newsUseCase.getNewsItemScrap.fromApi())
-                NewsItemWebScrap.set(newsUseCase.getNewsItemWebScrap.fromApi())
+                setServerScript(serverUseCase.serverScript.fromApi())
+                setAnimeBaseScrap(directoryUseCase.getDirectoryScrap.fromApi())
+                setAnimeProfileScrap(profileUseCase.getProfileScrap.fromApi())
+                setChapterHomeScrap(homeUseCase.getHomeScrap.fromApi())
+                setChapterScrap(chapterUseCase.getChapterScrap.fromApi())
+                setNewsItemScrap(newsUseCase.getNewsItemScrap.fromApi())
+                setNewsItemWebScrap(newsUseCase.getNewsItemWebScrap.fromApi())
 
                 Result.success()
             } catch (ex: IOException) {
@@ -43,5 +57,33 @@ class ScrapperWorker  @AssistedInject constructor(
                 Result.failure()
             }
         }
+    }
+
+    private suspend fun setServerScript(serverCode : String) {
+        preferences.set(Server.PREFERENCE_SERVER_SCRIPT,serverCode)
+    }
+
+    private suspend fun setAnimeBaseScrap(animeBaseScrap: AnimeBaseScrap) {
+        preferences.set(AnimeBaseScrap.INSTANCE,gson.toJson(animeBaseScrap))
+    }
+
+    private suspend fun setAnimeProfileScrap(animeProfileScrap: AnimeProfileScrap) {
+        preferences.set(AnimeProfileScrap.INSTANCE,gson.toJson(animeProfileScrap))
+    }
+
+    private suspend fun setChapterHomeScrap(chapterHomeScrap: ChapterHomeScrap) {
+        preferences.set(ChapterHomeScrap.INSTANCE,gson.toJson(chapterHomeScrap))
+    }
+
+    private suspend fun setChapterScrap(chapterScrap: ChapterScrap) {
+        preferences.set(ChapterScrap.INSTANCE,gson.toJson(chapterScrap))
+    }
+
+    private suspend fun setNewsItemScrap(newsItemScrap: NewsItemScrap) {
+        preferences.set(NewsItemScrap.INSTANCE,gson.toJson(newsItemScrap))
+    }
+
+    private suspend fun setNewsItemWebScrap(newsItemWebScrap: NewsItemWebScrap) {
+        preferences.set(NewsItemWebScrap.INSTANCE,gson.toJson(newsItemWebScrap))
     }
 }
