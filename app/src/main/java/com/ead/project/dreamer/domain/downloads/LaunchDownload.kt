b265f1442b0a6c92186e6db6283edd8e5af2b537
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:05:04 -0500

    launchDownload Class to launch respective layout

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/LaunchDownload.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/LaunchDownload.kt
@@ -1,17 +1,26 @@
 package com.ead.project.dreamer.domain.downloads
 
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.presentation.chapter.settings.ChapterSettingsFragment
 import javax.inject.Inject
 
-class LaunchDownload @Inject constructor(
-    private val createDownloadRequest: CreateDownloadRequest,
-    private val configureDownloadRequest: ConfigureDownloadRequest
-) {
+class LaunchDownload @Inject constructor() {
 
-    operator fun invoke(chapter : Chapter?,url : String) =
-        chapter?.let { createDownloadRequest(configureDownloadRequest(it,url),it.id)  }
+    private val bundle = Bundle()
 
-    operator fun invoke(title: String, url: String) {
-        createDownloadRequest(configureDownloadRequest(title, url))
+    operator fun invoke(context: Context, chapter: Chapter,isChapter : Boolean) {
+        val fragmentManager = (context as FragmentActivity).supportFragmentManager
+        val chapterSettings = ChapterSettingsFragment()
+        chapterSettings.apply {
+            arguments = bundle.apply {
+                clear()
+                putParcelable(Chapter.REQUESTED, chapter)
+                putBoolean(ChapterSettingsFragment.IS_INSTANCE_A_CHAPTER,isChapter)
+            }
+            show(fragmentManager, ChapterSettingsFragment.FRAGMENT)
+        }
     }
 }
