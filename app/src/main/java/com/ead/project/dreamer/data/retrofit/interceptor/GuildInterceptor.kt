e1b817a3796dfb53c1e5ec99656549e6a0ca46b0
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 09:50:09 -0500

    add work with discord preferences

--- app/src/main/java/com/ead/project/dreamer/data/retrofit/interceptor/GuildInterceptor.kt
+++ app/src/main/java/com/ead/project/dreamer/data/retrofit/interceptor/GuildInterceptor.kt
@@ -1,7 +1,8 @@
 package com.ead.project.dreamer.data.retrofit.interceptor
 
-import com.ead.project.dreamer.data.models.discord.Discord
-import com.ead.project.dreamer.data.utils.DataStore
+import android.util.Log
+import com.ead.project.dreamer.app.data.discord.Discord
+import com.ead.project.dreamer.app.data.discord.DiscordEAD
 import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -13,15 +14,18 @@ import okhttp3.Response
 class GuildInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return try {
-            val accessToken = DataStore.readString(Discord.ACCESS_TOKEN)
+            var request = chain.request()
+            val accessToken = Discord.getDiscordToken()?.access_token?:return chain.proceed(request)
+            Log.d("testing", "GuildInterceptor: $accessToken")
+
             val tObject = Gson().
-            toJson(AccessObject(accessToken,arrayListOf(Discord.BASIC_RANK_ID)))
+            toJson(AccessObject(accessToken,arrayListOf(DiscordEAD.BASIC_RANK_ID)))
 
             val mediaType = "application/json".toMediaType()
             val body = tObject.toString().toRequestBody(mediaType)
 
-            val request = chain.request().newBuilder()
-                .header("Authorization","Bot ${Discord.BOT_TOKEN}")
+            request = chain.request().newBuilder()
+                .header("Authorization","Bot ${DiscordEAD.BOT_TOKEN}")
                 .put(body)
                 .build()
 
