f1dbd8eb080013f634eaa1dc5de231ec8135ed1a
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:36:23 -0500

    todo token accessibility

--- app/src/main/java/com/ead/project/dreamer/data/retrofit/interceptor/UserAccessInterceptor.kt
+++ app/src/main/java/com/ead/project/dreamer/data/retrofit/interceptor/UserAccessInterceptor.kt
@@ -1,15 +1,14 @@
 package com.ead.project.dreamer.data.retrofit.interceptor
 
-import com.ead.project.dreamer.data.models.discord.Discord
-import com.ead.project.dreamer.data.utils.DataStore
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class UserAccessInterceptorInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return try {
-            val token = DataStore.readString(Discord.USER_TOKEN)
             var request = chain.request()
+            val token = "todo" //todo handle the user token
+
             val requestBuilder = request.newBuilder()
             request = requestBuilder
                 .header("Authorization",token)
