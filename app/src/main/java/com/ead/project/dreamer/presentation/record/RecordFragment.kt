fbb1a3cc07549f986fcbbe184638951edf87c35c
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:03:09 -0500

    renamed packages, injected variables in viewModel, and formatted code

--- app/src/main/java/com/ead/project/dreamer/presentation/record/RecordFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/record/RecordFragment.kt
@@ -1,46 +1,41 @@
-package com.ead.project.dreamer.ui.record
+package com.ead.project.dreamer.presentation.record
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ead.commons.lib.metrics.getAvailableWidthReference
 import com.ead.project.dreamer.R
-import com.ead.project.dreamer.data.models.discord.User
-import com.ead.project.dreamer.data.utils.AdManager
+import com.ead.project.dreamer.data.models.discord.DiscordUser
 import com.ead.project.dreamer.databinding.FragmentRecordsBinding
-import com.ead.project.dreamer.ui.record.adapters.BannerViewHolderAd
-import com.ead.project.dreamer.ui.record.adapters.ChapterRecordRecyclerViewAdapter
+import com.ead.project.dreamer.presentation.record.adapters.BannerViewHolderAd
+import com.ead.project.dreamer.presentation.record.adapters.ChapterRecordRecyclerViewAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class RecordFragment : Fragment() {
 
-    private lateinit var recordViewModel: RecordViewModel
+    private lateinit var viewModel: RecordViewModel
+
     private lateinit var adapter : ChapterRecordRecyclerViewAdapter
-    private var isLinear = false
+    private var isSmallDevice = false
 
     private var _binding : FragmentRecordsBinding?=null
     private val binding get() = _binding!!
     private lateinit var bannerViewHolderAd: BannerViewHolderAd
-    private var adManager : AdManager?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        recordViewModel = ViewModelProvider(this)[RecordViewModel::class.java]
+        viewModel = ViewModelProvider(this)[RecordViewModel::class.java]
     }
 
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        adManager?.onViewStateRestored()
-        super.onViewStateRestored(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,37 +47,39 @@ class RecordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        prepareAdSettings()
-        prepareLayout()
+        initAdSettings()
+        initLayouts()
         setupAds()
     }
 
-    private fun prepareLayout() {
+    private fun initLayouts() {
         bannerViewHolderAd = BannerViewHolderAd(binding.banner)
         binding.banner.root.visibility = View.GONE
         prepareRecyclerView()
     }
 
-    private fun prepareAdSettings() {
-        adManager = AdManager(
-            context = requireContext(),
-            adId = getString(R.string.ad_unit_id_native_records))
-        adManager?.setUp(User.isNotVip())
+    private fun initAdSettings() {
+        viewModel.adManager.setUp(
+            returnCase = DiscordUser.isVip(),
+            adId = getString(R.string.ad_unit_id_native_records)
+        )
     }
 
     private fun prepareRecyclerView() {
         binding.list.apply {
+
             layoutManager = getLayoutManagerMode()
-            this@RecordFragment.adapter = ChapterRecordRecyclerViewAdapter(activity as Context,isLinear)
+            this@RecordFragment.adapter = ChapterRecordRecyclerViewAdapter(activity as Context, isSmallDevice, viewModel.handleChapter, viewModel.launchDownload)
             adapter = this@RecordFragment.adapter
             setupRecords()
+
         }
     }
 
     private fun getLayoutManagerMode() : RecyclerView.LayoutManager =
         when(getAvailableWidthReference(380)) {
             0 -> {
-                isLinear = true
+                isSmallDevice = true
                 binding.list.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
                 LinearLayoutManager(requireContext())
             }
@@ -90,26 +87,33 @@ class RecordFragment : Fragment() {
         }
 
     private fun setupRecords() {
-        recordViewModel.getLiveDataRecords().observe(viewLifecycleOwner) {
-            if (it.isNotEmpty()) adapter.submitList(it)
+        viewModel.getLiveDataRecords().observe(viewLifecycleOwner) {
+
+            if (it.isNotEmpty()) {
+                adapter.submitList(it)
+            }
             else {
                 binding.txvIsEmpty.visibility = View.VISIBLE
                 binding.list.visibility = View.GONE
             }
-            if (recordViewModel.checkIfUpgradeExist(it)) recordViewModel.updateContinuation(it)
+            if (viewModel.checkIfUpgradeExist(it)) {
+                viewModel.updateContinuation(it)
+            }
+
         }
     }
 
     private fun setupAds() {
-        adManager?.getAd()?.observe(viewLifecycleOwner) {
+        viewModel.adManager.getItem().observe(viewLifecycleOwner) {
+
+            bannerViewHolderAd.bindTo(it?:return@observe)
             binding.banner.root.visibility = View.VISIBLE
-            bannerViewHolderAd.bindTo(it)
+
         }
     }
 
     override fun onDestroy() {
-        adManager?.onDestroy()
-        adManager = null
+        viewModel.adManager.onDestroy()
         super.onDestroy()
     }
 }
