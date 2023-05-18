da505d9943b695c436559d2cf0ab6494573631e4
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:40:57 -0500

    renamed class and added preference to mage onScale

---
+++ app/src/main/java/com/ead/project/dreamer/data/utils/ui/PlayerOnScaleGestureListener.kt
@@ -0,0 +1,45 @@
+package com.ead.project.dreamer.data.utils.ui
+
+import android.view.ScaleGestureDetector
+import com.ead.project.dreamer.data.commons.Constants
+import com.ead.project.dreamer.domain.PreferenceUseCase
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+class PlayerOnScaleGestureListener(
+    private val player: StyledPlayerView,
+    preferenceUseCase: PreferenceUseCase
+) : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+    private val preferences = preferenceUseCase.preferences
+    private val scope : CoroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
+    private var scaleFactor = 0f
+    var isHorizontalMode : Boolean = false
+
+    override fun onScale(detector: ScaleGestureDetector): Boolean {
+        scaleFactor = detector.scaleFactor
+        return true
+    }
+    override fun onScaleBegin(detector: ScaleGestureDetector) : Boolean =  true
+
+    override fun onScaleEnd(detector: ScaleGestureDetector) {
+        scope.launch {
+            if (isHorizontalMode)
+                if (scaleFactor > 1) {
+                    when(preferences.getInt(Constants.PREFERENCE_RESIZING_MODE, -1)) {
+                        -1 -> player.resizeMode =
+                            AspectRatioFrameLayout.RESIZE_MODE_FILL
+                        else -> player.resizeMode =
+                            preferences.getInt(Constants.PREFERENCE_RESIZING_MODE)
+                    }
+                } else {
+                    player.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                }
+        }
+    }
+}
