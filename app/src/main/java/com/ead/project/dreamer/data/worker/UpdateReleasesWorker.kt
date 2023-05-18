a2bd24ac752cd204fc405d0c1bf0dbf066807e99
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:35:56 -0500

    updated packages, and added preferences detect if directory is synchronized

--- app/src/main/java/com/ead/project/dreamer/data/worker/UpdateReleasesWorker.kt
+++ app/src/main/java/com/ead/project/dreamer/data/worker/UpdateReleasesWorker.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.ead.project.dreamer.data.commons.Constants
+import com.ead.project.dreamer.app.data.preference.Settings
 import com.ead.project.dreamer.data.network.WebProvider
-import com.ead.project.dreamer.data.utils.DataStore
 import com.ead.project.dreamer.domain.ObjectUseCase
+import com.ead.project.dreamer.domain.PreferenceUseCase
 import com.ead.project.dreamer.domain.ProfileUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -22,15 +22,19 @@ class UpdateReleasesWorker @AssistedInject constructor(
     @Assisted workerParameters: WorkerParameters,
     private val objectUseCase: ObjectUseCase,
     private val profileUseCase: ProfileUseCase,
-    private val webProvider: WebProvider
+    private val webProvider: WebProvider,
+    preferenceUseCase: PreferenceUseCase
 ) : CoroutineWorker(context,workerParameters) {
+
+    private val preferences = preferenceUseCase.preferences
 
     override suspend fun doWork(): Result {
         return withContext(Dispatchers.IO) {
             try {
                 val repositoryData = profileUseCase.getProfilesReleases()
-
-                if (DataStore.readBoolean(Constants.PREFERENCE_DIRECTORY_PROFILE)) {
+                val isDirectorySynchronized = preferences.getBoolean(Settings.SYNC_DIRECTORY_PROFILE)
+                
+                if (isDirectorySynchronized) {
                     for (pos in repositoryData.indices) {
                         val profile = repositoryData[pos]
                         val profileInProgress = async {
