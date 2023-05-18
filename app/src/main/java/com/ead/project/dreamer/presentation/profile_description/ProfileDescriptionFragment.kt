31595ab5681080ab8d702d43257951805957b1ea
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:52:42 -0500

    fragment that hold description referent from his profiles

---
+++ app/src/main/java/com/ead/project/dreamer/presentation/profile_description/ProfileDescriptionFragment.kt
@@ -0,0 +1,127 @@
+package com.ead.project.dreamer.presentation.profile_description
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ead.commons.lib.views.addSelectableItemEffect
+import com.ead.commons.lib.views.justifyInterWord
+import com.ead.project.dreamer.R
+import com.ead.project.dreamer.data.database.model.AnimeProfile
+import com.ead.project.dreamer.databinding.FragmentProfileDescriptionBinding
+import com.ead.project.dreamer.presentation.profile.AnimeProfileViewModel
+import com.ead.project.dreamer.presentation.profile.adapters.GenreRecyclerViewAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileDescriptionFragment : Fragment() {
+
+    private val viewModel : AnimeProfileViewModel by viewModels()
+
+    private val minLettersCharacter = 240
+    private var descriptionOverloaded = false
+    private var wrappedDescription = false
+
+    var profileId : Int = -1
+
+    private var _binding : FragmentProfileDescriptionBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileDescriptionBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    private var count = 0
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+            recyclerViewGenres.apply {
+                layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+            }
+            txvDescriptionContent.justifyInterWord()
+            txvDescriptionContent.addSelectableItemEffect()
+
+        }
+
+        viewModel.getAnimeProfile(profileId).observe(viewLifecycleOwner){ animeProfile ->
+
+            if (animeProfile != null) {
+                if (++count == 1) {
+                    loadAnimeProfileDetails(animeProfile)
+                }
+            }
+
+        }
+    }
+
+    private fun loadAnimeProfileDetails(animeProfile: AnimeProfile) {
+        binding.apply {
+
+            recyclerViewGenres.adapter = GenreRecyclerViewAdapter(animeProfile.genres,requireContext())
+            txvState.text = animeProfile.state
+            txvDate.text = animeProfile.date
+            txvRating.text = animeProfile.rating.toString()
+            ratingBar.rating = animeProfile.rating
+
+            if (animeProfile.description.length > minLettersCharacter) {
+                descriptionOverloaded = true
+                wrappedDescription(animeProfile,false)
+            } else {
+                txvDescriptionContent.text = animeProfile.description
+            }
+
+            txvDescriptionContent.setOnClickListener{
+                if (descriptionOverloaded) {
+                    val params = txvDescriptionContent.layoutParams as ViewGroup.LayoutParams
+                    if (wrappedDescription) {
+                        wrappedDescription(animeProfile,false)
+                        params.height = resources.getDimensionPixelSize(R.dimen.dimen_115dp)
+                    }
+                    else {
+                        wrappedDescription(animeProfile,true)
+                        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+                    }
+                    txvDescriptionContent.layoutParams = params
+                    wrappedDescription = !wrappedDescription
+                }
+            }
+
+        }
+    }
+
+    private fun wrappedDescription(animeProfile: AnimeProfile, wrapContent : Boolean) {
+        val text : String = animeProfile.description.substring(0,minLettersCharacter)
+        binding.apply {
+
+            if(!wrapContent) {
+                txvDescriptionContent
+                    .text = HtmlCompat.fromHtml(
+                    "$text<font color='Cyan'> <u>Mostrar más...</u></font>",
+                    HtmlCompat.FROM_HTML_MODE_LEGACY
+                )
+            }
+            else {
+                txvDescriptionContent
+                    .text = HtmlCompat.fromHtml(
+                    "${animeProfile.description}<font color='Red'> <u>Mostrar menos...</u></font>",
+                    HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
