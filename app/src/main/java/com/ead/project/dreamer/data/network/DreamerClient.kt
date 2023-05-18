a35c164602cf066b25fe34b58763f8d5757a1d0f
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:14:24 -0500

    renamed references

--- app/src/main/java/com/ead/project/dreamer/data/network/DreamerClient.kt
+++ app/src/main/java/com/ead/project/dreamer/data/network/DreamerClient.kt
@@ -3,22 +3,22 @@ package com.ead.project.dreamer.data.network
 import android.graphics.Bitmap
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import com.ead.project.dreamer.data.commons.Constants
-import com.ead.project.dreamer.data.utils.ThreadUtil
+import com.ead.project.dreamer.app.data.util.HttpUtil
+import com.ead.project.dreamer.data.utils.Thread
 
 open class DreamerClient : WebViewClient() {
 
     var timeout = true
     var timesLoaded = 0
-    private fun run(task: () -> Unit) = ThreadUtil.runInMs(task, 10000)
-    fun onUi(task: () -> Unit) = ThreadUtil.onUi { task() }
+    private fun run(task: () -> Unit) = Thread.runInMs(task, 10000)
+    fun onUi(task: () -> Unit) = Thread.onUi { task() }
 
     @Deprecated("OnPageInit option")
     override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
         super.onPageStarted(view, url, favicon)
         onPageInit(view, url, favicon)
         timeout = true
-        run { if (timeout && url != Constants.BLANK_BROWSER) onTimeout(view, url, favicon) }
+        run { if (timeout && url != HttpUtil.BLANK_BROWSER) onTimeout(view, url, favicon) }
     }
 
     @Deprecated("OnPageLoaded option")
@@ -26,7 +26,7 @@ open class DreamerClient : WebViewClient() {
         super.onPageFinished(view, url)
         timeout = false
         timesLoaded++
-        if (url != Constants.BLANK_BROWSER) onPageLoaded(view, url)
+        if (url != HttpUtil.BLANK_BROWSER) onPageLoaded(view, url)
     }
 
     open fun onPageInit(view: WebView?, url: String?, favicon: Bitmap?) {}
