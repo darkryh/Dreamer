fd7e23ca09991c91e9552821c750403a33bfb2ef
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:24:54 -0500

    injected MiniControllerFragment to work with CastManager class

--- app/src/main/java/com/ead/project/dreamer/presentation/player/cast/MiniControllerFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/player/cast/MiniControllerFragment.kt
@@ -1,19 +1,20 @@
-package com.ead.project.dreamer.ui.player.cast
+package com.ead.project.dreamer.presentation.player.cast
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import com.ead.project.dreamer.data.utils.media.CastManager
+import com.ead.project.dreamer.app.data.player.casting.CastManager
 import com.ead.project.dreamer.data.utils.ui.DreamerUIMiniController
 import dagger.hilt.android.AndroidEntryPoint
 import com.google.android.gms.cast.framework.media.widget.MiniControllerFragment
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MiniControllerFragment : MiniControllerFragment() {
 
-    private var castManager : CastManager = CastManager(true)
+    @Inject lateinit var castManager : CastManager
     private lateinit var dreamerUIMiniController: DreamerUIMiniController
     private lateinit var customButtonView : ImageView
 
