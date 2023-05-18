3da23b300b682c1ed5834eac62a9597b38c40785
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:18:50 -0500

    object that hold constants settings

---
+++ app/src/main/java/com/ead/project/dreamer/app/data/preference/Settings.kt
@@ -0,0 +1,9 @@
+package com.ead.project.dreamer.app.data.preference
+
+object Settings {
+
+    const val APP_VERSION = "APP_VERSION"
+    const val SYNC_DIRECTORY_PROFILE = "SYNC_DIRECTORY_PROFILE"
+
+    const val RESET_DOWNLOADS = "RESET_DOWNLOAD"
+}
