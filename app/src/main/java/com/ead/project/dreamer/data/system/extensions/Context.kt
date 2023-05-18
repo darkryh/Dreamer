47f05bcdfc8c8995d18564dc0dc1e245a17f7afa
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 23:45:46 -0500

    context class extra.

--- app/src/main/java/com/ead/project/dreamer/data/system/extensions/Context.kt
+++ app/src/main/java/com/ead/project/dreamer/data/system/extensions/Context.kt
@@ -2,7 +2,12 @@ package com.ead.project.dreamer.data.system.extensions
 
 import android.content.Context
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 fun Context.toast(text : String,duration : Int = Toast.LENGTH_LONG) {
     Toast.makeText(this,text,duration).show()
+}
+
+fun Fragment.toast(text : String,duration : Int = Toast.LENGTH_LONG) {
+    requireActivity().toast(text, duration)
 }
