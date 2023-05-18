9e8485a3398fa95d11e32ebf061a91c324408e75
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 23:53:43 -0500

    renamed references

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/CreateManualDownload.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/CreateManualDownload.kt
@@ -3,14 +3,14 @@ package com.ead.project.dreamer.domain.downloads
 import com.ead.project.dreamer.data.database.model.Chapter
 import javax.inject.Inject
 
-class LaunchManualDownload @Inject constructor(
+class CreateManualDownload @Inject constructor(
     private val downloadEngine: DownloadEngine,
-    private val launchDownload: LaunchDownload
+    private val createDownload: CreateDownload
 ) {
 
     operator fun invoke(chapter : Chapter, url : String) {
         downloadEngine.settingBroadcast()
-        launchDownload(chapter, url)
+        createDownload(chapter, url)
     }
 
 }
