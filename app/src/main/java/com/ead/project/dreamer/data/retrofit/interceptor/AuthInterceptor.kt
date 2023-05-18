faa075202c295382e61a33c5ec9355f2357530bf
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 23:29:43 -0500

    added getter code by the discord preferences

--- app/src/main/java/com/ead/project/dreamer/data/retrofit/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/ead/project/dreamer/data/retrofit/interceptor/AuthInterceptor.kt
@@ -1,14 +1,14 @@
 package com.ead.project.dreamer.data.retrofit.interceptor
 
-import com.ead.project.dreamer.data.models.discord.Discord
-import com.ead.project.dreamer.data.utils.DataStore
+import com.ead.project.dreamer.app.data.discord.Discord
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return try {
-            val accessToken = DataStore.readString(Discord.ACCESS_TOKEN)
+            val accessToken = Discord.getDiscordToken()?.access_token
+
             val request = chain.request().newBuilder()
                 .addHeader("Authorization" , "Bearer $accessToken")
                 .addHeader("Content-Type" , "application/x-www-form-urlencoded")
