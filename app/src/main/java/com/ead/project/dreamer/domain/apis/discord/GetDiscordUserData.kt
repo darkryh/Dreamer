d5eb8c5e0bc6e9af1160f0d9bbcda837eee915c6
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:25:24 -0500

    renamed to GetDiscordUserData and references

--- app/src/main/java/com/ead/project/dreamer/domain/apis/discord/GetDiscordUserData.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/apis/discord/GetDiscordUserData.kt
@@ -3,7 +3,7 @@ package com.ead.project.dreamer.domain.apis.discord
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.ead.project.dreamer.data.AnimeRepository
-import com.ead.project.dreamer.data.models.discord.User
+import com.ead.project.dreamer.data.models.discord.DiscordUser
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,23 +13,23 @@ class GetDiscordUserData @Inject constructor(
     private val repository: AnimeRepository
 ) {
 
-    fun livedata () :  MutableLiveData<User?> = getUserData()
+    fun livedata () :  MutableLiveData<DiscordUser?> = getUserData()
 
-    private var user : MutableLiveData<User?>?= null
+    private var discordUser : MutableLiveData<DiscordUser?>?= null
 
-    private fun getUserData() : MutableLiveData<User?> {
-        user?.value = null
+    private fun getUserData() : MutableLiveData<DiscordUser?> {
+        discordUser?.value = null
         val discordService = repository.getDiscordService(repository.getDiscordAuthRetrofit())
-        val response : Call<User?> = discordService.getCurrentUser()
-        response.enqueue(object : Callback<User?> {
-            override fun onResponse(call: Call<User?>, response: Response<User?>) {
-                try { if (response.isSuccessful) user?.value = response.body() }
+        val response : Call<DiscordUser?> = discordService.getCurrentUser()
+        response.enqueue(object : Callback<DiscordUser?> {
+            override fun onResponse(call: Call<DiscordUser?>, response: Response<DiscordUser?>) {
+                try { if (response.isSuccessful) discordUser?.value = response.body() }
                 catch ( e : Exception) { e.printStackTrace() }
             }
-            override fun onFailure(call: Call<User?>, t: Throwable) {
+            override fun onFailure(call: Call<DiscordUser?>, t: Throwable) {
                 Log.e("error", "onFailure: ${t.cause?.message.toString()}", )
             }
         })
-        return user?:MutableLiveData<User?>().also { user = it }
+        return discordUser?:MutableLiveData<DiscordUser?>().also { discordUser = it }
     }
 }
