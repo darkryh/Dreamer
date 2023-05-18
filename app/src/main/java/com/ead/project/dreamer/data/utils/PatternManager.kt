27a388afe3ad8e41d50136033783a03c2d4b365e
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:34:26 -0500

    converted to object

--- app/src/main/java/com/ead/project/dreamer/data/utils/PatternManager.kt
+++ app/src/main/java/com/ead/project/dreamer/data/utils/PatternManager.kt
@@ -2,27 +2,24 @@ package com.ead.project.dreamer.data.utils
 
 import java.util.regex.Pattern
 
-class PatternManager {
+object PatternManager {
 
-    companion object {
+    fun singleMatch(string: String, regex : String, groupIndex : Int = 1): String? {
+        val pattern = Pattern.compile(regex, Pattern.MULTILINE)
+        val matcher = pattern.matcher(string)
+        return if (matcher.find()) {
+            matcher.group(groupIndex)
+        } else null
+    }
 
-        fun singleMatch(url: String, regex : String, groupIndex : Int = 1): String? {
-            val pattern = Pattern.compile(regex, Pattern.MULTILINE)
-            val matcher = pattern.matcher(url)
-            return if (matcher.find()) {
-                matcher.group(groupIndex)
-            } else null
+    fun multipleMatches(string: String, regex: String, groupIndex: Int=1): List<String> {
+        val pattern = Pattern.compile(regex, Pattern.MULTILINE)
+        val matcher = pattern.matcher(string)
+        val stringArrayList = ArrayList<String>()
+        while (matcher.find()) {
+            matcher.group(groupIndex)?.let { stringArrayList.add(it) }
         }
-
-        fun multipleMatches(string: String, regex: String, groupIndex: Int=1): List<String> {
-            val pattern = Pattern.compile(regex, Pattern.MULTILINE)
-            val matcher = pattern.matcher(string)
-            val stringArrayList = ArrayList<String>()
-            while (matcher.find()) {
-                matcher.group(groupIndex)?.let { stringArrayList.add(it) }
-            }
-            if (stringArrayList.isEmpty()) throw RuntimeException("null")
-            return stringArrayList
-        }
+        if (stringArrayList.isEmpty()) throw RuntimeException("null")
+        return stringArrayList
     }
 }
