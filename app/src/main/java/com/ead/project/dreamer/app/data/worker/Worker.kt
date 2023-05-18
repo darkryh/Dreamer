4fe1d042778b6d0c604670ead44ddd2037eb4e6c
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:36:54 -0500

    worker object that hold all references for workers

---
+++ app/src/main/java/com/ead/project/dreamer/app/data/worker/Worker.kt
@@ -0,0 +1,35 @@
+package com.ead.project.dreamer.app.data.worker
+
+object Worker {
+
+    const val SYNC_DIRECTORY = "CONSTANT_SYNC_DIRECTORY"
+
+    const val SYNC_HOME = "CONSTANT_SYNC_HOME"
+
+    const val SYNC_NEWS = "SYNC_NEWS"
+
+    const val SYNC_NEW_CONTENT = "SYNC_NEW_CONTENT"
+
+    const val SYNC_RELEASES = "SYNC_RELEASES"
+
+    const val SYNC_NOTIFICATIONS_FIRST_TIME = "SYNC_NOTIFICATIONS_FIRST_TIME"
+
+    const val SYNC_SCRAPPER = "SYNC_SCRAPPER"
+
+    const val SYNC_PROFILE_CHECKER = "SYNC_PROFILE_CHECKER"
+
+    const val SYNC_CHAPTER_SIZE = "SYNC_CHAPTER_SIZE"
+
+    const val SYNC_PROFILE_FIXER_CHECKER = "SYNC_PROFILE_FIXER_CHECKER"
+
+    const val SYNC_CHAPTER_FIXER_SIZE = "SYNC_CHAPTER_FIXER_SIZE"
+
+
+    //key workers to get some value
+
+    const val DIRECTORY_KEY = "DIRECTORY_KEY"
+
+    const val ANIME_PROFILE_KEY = "ANIME_PROFILE_KEY"
+
+    const val CHAPTER_PROFILE_KEY = "CHAPTER_PROFILE_KEY"
+}
