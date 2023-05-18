128d5a619930b7f3fbc72e57966f7d7820ef52ec
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:31:59 -0500

    updated preferences and todo manage delete downloads

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/StartManualDownload.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/StartManualDownload.kt
@@ -5,35 +5,37 @@ import android.widget.Toast
 import com.ead.project.dreamer.R
 import com.ead.project.dreamer.data.database.model.Chapter
 import com.ead.project.dreamer.data.system.extensions.toast
-import com.ead.project.dreamer.data.utils.ThreadUtil
+import com.ead.project.dreamer.data.utils.Thread
+import com.ead.project.dreamer.domain.servers.LaunchServer
 import javax.inject.Inject
 
 class StartManualDownload @Inject constructor(
     private val context: Context,
     private val isInDownloadProgress: IsInDownloadProgress,
+    private val launchServer: LaunchServer,
     private val getDownloads: GetDownloads,
     private val removeDownload: RemoveDownload
 ) {
 
     operator fun invoke(chapter: Chapter,mContext: Context) {
-        when (chapter.downloadState) {
-            Chapter.DOWNLOAD_STATUS_INITIALIZED -> {
+        when (chapter.state) {
+            Chapter.STATUS_STREAMING -> {
                 if (!isInDownloadProgress(chapter))
-                    Chapter.launchServer(mContext, chapter, true)
+                    launchServer(mContext, chapter,true)
                 else toast(context.getString(R.string.warning_chapter_status_in_progress))
             }
-            Chapter.DOWNLOAD_STATUS_RUNNING -> toast(context.getString(R.string.warning_chapter_status_in_progress))
-            Chapter.DOWNLOAD_STATUS_PENDING -> toast(context.getString(R.string.warning_chapter_status_pending))
-            Chapter.DOWNLOAD_STATUS_PAUSED -> toast(context.getString(R.string.warning_chapter_status_paused))
-            Chapter.DOWNLOAD_STATUS_FAILED -> {
-                removeDownload(getDownloads().singleOrNull{ it.second == chapter.id })
-                Chapter.launchServer(mContext, chapter, true)
+            Chapter.STATUS_RUNNING -> toast(context.getString(R.string.warning_chapter_status_in_progress))
+            Chapter.STATUS_PENDING -> toast(context.getString(R.string.warning_chapter_status_pending))
+            Chapter.STATUS_PAUSED -> toast(context.getString(R.string.warning_chapter_status_paused))
+            Chapter.STATUS_FAILED -> {
+                //removeDownload(getDownloads().singleOrNull{ it.second == chapter.id })
+                launchServer(mContext, chapter,true)
                 toast(context.getString(R.string.warning_chapter_status_failed))
             }
-            Chapter.DOWNLOAD_STATUS_COMPLETED -> toast(context.getString(R.string.warning_chapter_status_completed))
+            Chapter.STATUS_DOWNLOADED -> toast(context.getString(R.string.warning_chapter_status_completed))
         }
     }
 
     private fun toast(string: String) = runOnUI { context.toast(string,Toast.LENGTH_SHORT) }
-    private fun runOnUI(task: () -> Unit) = ThreadUtil.onUi { task() }
+    private fun runOnUI(task: () -> Unit) = Thread.onUi { task() }
 }
