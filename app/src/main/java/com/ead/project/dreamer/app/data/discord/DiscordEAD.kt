09d496b1c3c7e4207447b3f82e8df250fb182c18
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 23:56:56 -0500

    DiscordEad object that contains all about discord's developer

---
+++ app/src/main/java/com/ead/project/dreamer/app/data/discord/DiscordEAD.kt
@@ -0,0 +1,26 @@
+package com.ead.project.dreamer.app.data.discord
+
+import com.ead.project.dreamer.data.models.discord.DISCORD_BOT_TOKEN
+
+object DiscordEAD {
+
+    const val SERVER_ID = "934335404172275732"
+    const val BASIC_RANK_ID = "946849009262280734"
+
+    const val BOT_TOKEN = DISCORD_BOT_TOKEN
+
+
+    const val RANK_UNKNOWN = "Unknown"
+    const val RANK_USER = "User"
+    const val RANK_MPV = "Mvp"
+    const val RANK_VIP = "Vip"
+    const val RANK_ADMIN = "Admin"
+    const val RANK_OWNER = "Owner"
+
+    const val RANK_USER_ID = "946849009262280734"
+    const val RANK_MPV_ID = "953071633239785492"
+    const val RANK_VIP_ID = "953071933971374141"
+    const val RANK_ADMIN_ID = "934336329070833674"
+    const val RANK_OWNER_ID = "952259476864499782"
+
+}
