ad56a8e4b3d6dc1b1b92859c3abda939a77d9099
alex_ <alex_torres-xc@hotmail.com>
2023-05-17 23:55:40 -0500

    added state directory

--- app/src/main/java/com/ead/project/dreamer/domain/DirectoryUseCase.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/DirectoryUseCase.kt
@@ -3,10 +3,14 @@ package com.ead.project.dreamer.domain
 import com.ead.project.dreamer.domain.apis.app.GetDirectoryScrap
 import com.ead.project.dreamer.domain.databasequeries.GetDirectory
 import com.ead.project.dreamer.domain.databasequeries.GetDirectoryList
+import com.ead.project.dreamer.domain.directory.GetDirectoryState
+import com.ead.project.dreamer.domain.directory.SetDirectoryState
 import javax.inject.Inject
 
 class DirectoryUseCase @Inject constructor(
     val getDirectoryList: GetDirectoryList,
     val getDirectory: GetDirectory,
-    val getDirectoryScrap: GetDirectoryScrap
+    val getDirectoryScrap: GetDirectoryScrap,
+    val getDirectoryState: GetDirectoryState,
+    val setDirectoryState: SetDirectoryState
 )
