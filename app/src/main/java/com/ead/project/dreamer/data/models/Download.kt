4302fad4e450841207cb047431eebf1c38d20b28
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:03:12 -0500

    renamed class updated some functions removed and added

--- app/src/main/java/com/ead/project/dreamer/data/models/Download.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/Download.kt
@@ -1,9 +1,11 @@
 package com.ead.project.dreamer.data.models
 
 import android.app.DownloadManager
-import com.ead.project.dreamer.data.utils.DiffUtilEquality
+import com.ead.project.dreamer.app.data.files.Files
+import com.ead.project.dreamer.data.utils.ui.mechanism.EqualsDiffUtil
+import java.io.File
 
-data class DownloadItem(
+data class Download(
     var id : Long,
     val idReference : Int,
     val title : String,
@@ -12,11 +14,18 @@ data class DownloadItem(
     var current : Int,
     var total : Int,
     var state : Int
-) : DiffUtilEquality {
+) : EqualsDiffUtil {
 
     companion object {
         const val DOWNLOAD_TYPE_CHAPTER = 1
         const val DOWNLOAD_TYPE_UPDATE = 2
+    }
+
+    fun toApkFile() : File {
+        return File(
+            Files.DirectoryDownloadsFile,
+            "$title.apk"
+        )
     }
 
     fun isInProgress() =  state != DownloadManager.STATUS_FAILED &&
@@ -25,19 +34,19 @@ data class DownloadItem(
     override fun equalsHeader(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || javaClass != other.javaClass) return false
-        val downloadItem: DownloadItem = other as DownloadItem
-        return this.id == downloadItem.id
+        val download: Download = other as Download
+        return this.id == download.id
     }
 
     override fun equalsContent(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || javaClass != other.javaClass) return false
-        val downloadItem: DownloadItem = other as DownloadItem
-        return this.idReference == downloadItem.idReference
-                && this.title == downloadItem.title
-                && this.number == downloadItem.number
-                && this.type == downloadItem.type
-                && this.current == downloadItem.current
-                && this.state == downloadItem.state
+        val download: Download = other as Download
+        return this.idReference == download.idReference
+                && this.title == download.title
+                && this.number == download.number
+                && this.type == download.type
+                && this.current == download.current
+                && this.state == download.state
     }
 }
