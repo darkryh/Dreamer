f785e6f5b99a7c3713dcb23259c3d38bd3441984
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 09:57:43 -0500

    formatted code in binding

---
+++ app/src/main/java/com/ead/project/dreamer/presentation/inbox/InboxFragment.kt
@@ -0,0 +1,77 @@
+package com.ead.project.dreamer.presentation.inbox
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.ead.project.dreamer.databinding.FragmentInboxBinding
+import com.ead.project.dreamer.presentation.inbox.adapter.InboxViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class InboxFragment : Fragment() {
+
+    private lateinit var viewModel: InboxViewModel
+
+    private var _binding : FragmentInboxBinding?= null
+    private val binding get() = _binding!!
+
+    private lateinit var viewPager : InboxViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[InboxViewModel::class.java]
+        viewModel.adManager.restore()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInboxBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPager()
+        setupTabLayout()
+    }
+
+    private fun setupViewPager() {
+        binding.apply {
+            viewPager = InboxViewPagerAdapter(requireActivity(),viewPager2)
+            viewPager2.adapter = viewPager
+            viewPager2.registerOnPageChangeCallback(object  : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.tabLayout.getTabAt(position)?.select()
+                }
+            })
+        }
+    }
+
+    private fun setupTabLayout() {
+        binding.apply {
+            tabLayout.addOnTabSelectedListener(object  : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    if (tab != null) {
+                        viewPager2.currentItem = tab.position
+                    }
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
