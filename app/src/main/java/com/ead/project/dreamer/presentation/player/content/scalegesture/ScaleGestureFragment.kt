33d385c15a42207270d222b149d0d3dc1bc984db
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:09:38 -0500

    formatted binding code and added preferences

---
+++ app/src/main/java/com/ead/project/dreamer/presentation/player/content/scalegesture/ScaleGestureFragment.kt
@@ -0,0 +1,111 @@
+package com.ead.project.dreamer.presentation.player.content.scalegesture
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.ead.commons.lib.views.addSelectableItemEffect
+import com.ead.project.dreamer.app.data.util.system.hideSystemUI
+import com.ead.project.dreamer.app.data.util.system.setStateExpanded
+import com.ead.project.dreamer.app.data.util.system.setWidthMatchParent
+import com.ead.project.dreamer.data.commons.Constants
+import com.ead.project.dreamer.data.utils.Thread
+import com.ead.project.dreamer.databinding.BottomModalScaleGestureBinding
+import com.ead.project.dreamer.presentation.player.PlayerViewModel
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ScaleGestureFragment : BottomSheetDialogFragment() {
+
+    private val viewModel : PlayerViewModel by viewModels()
+
+    @Inject lateinit var scope: CoroutineScope
+    lateinit var playerView : StyledPlayerView
+
+    override fun onStart() {
+        super.onStart()
+        setWidthMatchParent()
+        setStateExpanded()
+    }
+
+    private var _binding : BottomModalScaleGestureBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BottomModalScaleGestureBinding.inflate(inflater, container, false)
+
+        initLayouts()
+        settingLayouts()
+        return binding.root
+    }
+
+    private fun initLayouts() {
+        binding.buttonStretch.addSelectableItemEffect()
+        binding.buttonZoom.addSelectableItemEffect()
+        binding.buttonFixedWidth.addSelectableItemEffect()
+    }
+
+    private fun settingLayouts() {
+        binding.buttonStretch.setOnClickListener {
+            execute {
+                scope.launch {
+                    viewModel.preferences.set(
+                        Constants.PREFERENCE_RESIZING_MODE,
+                        AspectRatioFrameLayout.RESIZE_MODE_FILL
+                    )
+                }
+            }
+        }
+
+        binding.buttonZoom.setOnClickListener {
+            execute {
+                scope.launch {
+                    viewModel.preferences.set(
+                        Constants.PREFERENCE_RESIZING_MODE,
+                        AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                    )
+                }
+            }
+        }
+
+        binding.buttonFixedWidth.setOnClickListener {
+            execute {
+                scope.launch {
+                    viewModel.preferences.set(
+                        Constants.PREFERENCE_RESIZING_MODE,
+                        AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH
+                    )
+                }
+            }
+        }
+    }
+
+    private fun execute(task :  () -> Unit ) {
+        Thread.runInMs({
+            task()
+            dismiss()
+        },175L)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activity?.hideSystemUI()
+    }
+
+}
