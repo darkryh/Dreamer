4817d33a3c6b2bdc4248c2ec5e5040b0a46d1b4b
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 09:48:36 -0500

    renamed references and packages

--- app/src/main/java/com/ead/project/dreamer/domain/servers/GetServer.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/GetServer.kt
@@ -1,6 +1,6 @@
 package com.ead.project.dreamer.domain.servers
-import com.ead.project.dreamer.data.commons.Constants
-import com.ead.project.dreamer.data.models.Server
+
+import com.ead.project.dreamer.app.data.server.Server
 import com.ead.project.dreamer.data.models.server.*
 import javax.inject.Inject
 
@@ -8,32 +8,32 @@ class GetServer @Inject constructor(
     private val serverIdentifier: ServerIdentifier
 ) {
 
-    operator fun invoke(url : String) : Server =
+    operator fun invoke(url : String) : com.ead.project.dreamer.data.models.Server =
         when (serverIdentifier(url)) {
-            Constants.TITLE_OKRU -> Okru(url)
-            Constants.TITLE_FEMBED -> Fembed(url)
-            Constants.TITLE_ONEFICHIER -> Onefichier(url)
-            Constants.TITLE_STREAMSB -> StreamSB(url)
-            Constants.TITLE_SENDVID -> Senvid(url)
-            Constants.TITLE_DOOD_STREAM -> DoodStream(url)
-            Constants.TITLE_SOLIDFILES -> SolidFiles(url)
-            Constants.TITLE_BAYFILES -> Bayfiles(url)
-            Constants.TITLE_VIDEOBIN -> Videobin(url)
-            Constants.TITLE_ZIPPYSHARE -> Zippyshare(url)
-            Constants.TITLE_MEDIAFIRE -> Mediafire(url)
-            Constants.TITLE_STREAMTAPE -> Streamtape(url)
-            Constants.TITLE_GOOGLE_DRIVE -> GoogleDrive(url)
-            Constants.TITLE_PUJ -> Puj(url)
-            Constants.TITLE_VOE -> Voe(url)
-            Constants.TITLE_UPTOBOX -> Uptobox(url)
-            Constants.TITLE_ANONFILE -> Anonfiles(url)
-            Constants.TITLE_MEGA_UP -> MegaUp(url)
-            Constants.TITLE_FIRELOAD -> Fireload(url)
-            Constants.TITLE_MP4UPLOAD -> Mp4Upload(url)
-            Constants.TITLE_UQLOAD -> Uqload(url)
-            Constants.TITLE_MEGA -> Mega(url)
-            Constants.TITLE_VIDLOX -> Vidlox(url)
-            Constants.TITLE_YOUR_UPLOAD -> YourUpload(url)
+            Server.OKRU -> Okru(url)
+            Server.FEMBED -> Fembed(url)
+            Server.ONEFICHIER -> Onefichier(url)
+            Server.STREAMSB -> StreamSB(url)
+            Server.SENDVID -> Senvid(url)
+            Server.DOOD_STREAM -> DoodStream(url)
+            Server.SOLIDFILES -> SolidFiles(url)
+            Server.BAYFILES -> Bayfiles(url)
+            Server.VIDEOBIN -> Videobin(url)
+            Server.ZIPPYSHARE -> Zippyshare(url)
+            Server.MEDIAFIRE -> Mediafire(url)
+            Server.STREAMTAPE -> Streamtape(url)
+            Server.GOOGLE_DRIVE -> GoogleDrive(url)
+            Server.PUJ -> Puj(url)
+            Server.VOE -> Voe(url)
+            Server.UPTOBOX -> Uptobox(url)
+            Server.ANONFILE -> Anonfiles(url)
+            Server.MEGA_UP -> MegaUp(url)
+            Server.FIRELOAD -> Fireload(url)
+            Server.MP4UPLOAD -> Mp4Upload(url)
+            Server.UQLOAD -> Uqload(url)
+            Server.MEGA -> Mega(url)
+            Server.VIDLOX -> Vidlox(url)
+            Server.YOUR_UPLOAD -> YourUpload(url)
             else -> NullServer(url)
         }
 }
