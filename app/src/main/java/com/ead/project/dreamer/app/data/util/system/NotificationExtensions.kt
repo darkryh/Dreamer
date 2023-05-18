14deb3eb0795140733790be6eb575569da7957a0
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:35:05 -0500

    Extensions provided by name

---
+++ app/src/main/java/com/ead/project/dreamer/app/data/util/system/NotificationExtensions.kt
@@ -0,0 +1,27 @@
+package com.ead.project.dreamer.app.data.util.system
+
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.URL
+
+fun NotificationCompat.Builder.loadImage(imageUrl: String) = runBlocking {
+    val url = URL(imageUrl)
+    withContext(Dispatchers.IO) {
+        try {
+            val input = url.openStream()
+            BitmapFactory.decodeStream(input)
+        } catch (e: IOException) {
+            null
+        }
+    }?.let { bitmap ->
+        setLargeIcon(bitmap)
+        setStyle(
+            NotificationCompat.BigPictureStyle()
+                .bigPicture(bitmap)
+                .bigLargeIcon(null))
+    }
+}
