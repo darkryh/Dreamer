e224b2bbb5ab50fc4091425052b9c166c7a5566e
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 10:33:06 -0500

    Notifications Constants

---
+++ app/src/main/java/com/ead/project/dreamer/app/data/notifications/Notifications.kt
@@ -0,0 +1,10 @@
+package com.ead.project.dreamer.app.data.notifications
+
+object Notifications {
+
+    const val HOME_INDEX = "NOTIFICATIONS_HOME_INDEX"
+    const val DEFAULT_HOME_INDEX : Int = 0
+    const val CONFIG = "NOTIFICATIONS_CONFIG"
+
+    const val MARKETING_ID = 1000
+}
