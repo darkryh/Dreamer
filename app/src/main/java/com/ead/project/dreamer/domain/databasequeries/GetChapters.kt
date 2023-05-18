619b34d75055f198d0cd9f4f8353859b54665b56
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 00:21:31 -0500

    removed unused section livedata and added query from number

--- app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetChapters.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/databasequeries/GetChapters.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import com.ead.project.dreamer.data.AnimeRepository
 import com.ead.project.dreamer.data.database.model.Chapter
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetChapters @Inject constructor(
@@ -15,8 +14,6 @@ class GetChapters @Inject constructor(
         if (isDesc) { repository.getFlowChaptersFromProfile(id) }
         else { repository.getFlowChaptersFromProfileAsc(id) }.asLiveData()
 
-    fun livedata (id : Int,start: Int,end: Int) : LiveData<List<Chapter>> =
-        repository.getFlowChaptersFromProfile(id).map {
-        it.filter { filter -> filter.number >= start }
-            .filter { filter2 -> filter2.number <= end } }.asLiveData()
+    fun fromNumber(id: Int,number : Int) : LiveData<List<Chapter>> =
+        repository.getFlowChaptersFromNumber(id,number).asLiveData()
 }
