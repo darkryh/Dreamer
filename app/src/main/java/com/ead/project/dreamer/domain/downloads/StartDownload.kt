05c721a27820ef2f78a727831df823f14c5e66f6
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 11:31:43 -0500

    updated preferences and todo manage delete downloads

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/StartDownload.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/StartDownload.kt
@@ -3,9 +3,10 @@ package com.ead.project.dreamer.domain.downloads
 import android.content.Context
 import android.widget.Toast
 import com.ead.project.dreamer.R
+import com.ead.project.dreamer.app.data.files.FilesPreferences
 import com.ead.project.dreamer.data.database.model.Chapter
 import com.ead.project.dreamer.data.system.extensions.toast
-import com.ead.project.dreamer.data.utils.ThreadUtil
+import com.ead.project.dreamer.data.utils.Thread
 import javax.inject.Inject
 
 class StartDownload @Inject constructor(
@@ -14,7 +15,8 @@ class StartDownload @Inject constructor(
     private val isInDownloadProgress: IsInDownloadProgress,
     private val addDownload: AddDownload,
     private val removeDownload: RemoveDownload,
-    private val getDownloads: GetDownloads
+    private val getDownloads: GetDownloads,
+    private val filesPreferences: FilesPreferences
 ) {
 
     operator fun invoke(chapters : List<Chapter>) {
@@ -26,24 +28,26 @@ class StartDownload @Inject constructor(
     }
 
     operator fun invoke(chapter: Chapter) {
-        when (chapter.downloadState) {
-            Chapter.DOWNLOAD_STATUS_INITIALIZED -> {
+        when (chapter.state) {
+            Chapter.STATUS_STREAMING -> {
                 if (!isInDownloadProgress(chapter)) {
                     addDownload(chapter)
                     toast(context.getString(R.string.warning_chapter_status_starting))
                 }
                 else toast(context.getString(R.string.warning_chapter_status_in_progress))
             }
-            Chapter.DOWNLOAD_STATUS_RUNNING -> toast(context.getString(R.string.warning_chapter_status_in_progress))
-            Chapter.DOWNLOAD_STATUS_PENDING -> toast(context.getString(R.string.warning_chapter_status_pending))
-            Chapter.DOWNLOAD_STATUS_PAUSED -> toast(context.getString(R.string.warning_chapter_status_paused))
-            Chapter.DOWNLOAD_STATUS_FAILED -> {
-                removeDownload(getDownloads().singleOrNull{ it.second == chapter.id })
+            Chapter.STATUS_RUNNING -> toast(context.getString(R.string.warning_chapter_status_in_progress))
+            Chapter.STATUS_PENDING -> toast(context.getString(R.string.warning_chapter_status_pending))
+            Chapter.STATUS_PAUSED -> toast(context.getString(R.string.warning_chapter_status_paused))
+            Chapter.STATUS_FAILED -> {
+                //removeDownload(getDownloads().singleOrNull{ it.second == chapter.id })
                 addDownload(chapter)
                 toast(context.getString(R.string.warning_chapter_status_failed))
             }
-            Chapter.DOWNLOAD_STATUS_COMPLETED -> {
-                if (chapter.getFile().exists()) toast(context.getString(R.string.warning_chapter_status_completed))
+            Chapter.STATUS_DOWNLOADED -> {
+                if (filesPreferences.getChapterFile(chapter).exists()) {
+                    toast(context.getString(R.string.warning_chapter_status_completed))
+                }
                 else {
                     addDownload(chapter)
                     toast(context.getString(R.string.warning_chapter_status_starting))
@@ -53,5 +57,5 @@ class StartDownload @Inject constructor(
     }
 
     private fun toast(string: String) = runOnUI { context.toast(string,Toast.LENGTH_SHORT) }
-    private fun runOnUI(task: () -> Unit) = ThreadUtil.onUi { task() }
+    private fun runOnUI(task: () -> Unit) = Thread.onUi { task() }
 }
