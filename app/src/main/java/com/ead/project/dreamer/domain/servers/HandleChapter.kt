90664c9ae78d10c14709755ecdd18adeb8c734be
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 22:40:55 -0500

    removed LaunchServer class, just using launch companion objet from the fragment

--- app/src/main/java/com/ead/project/dreamer/domain/servers/HandleChapter.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/HandleChapter.kt
@@ -2,19 +2,19 @@ package com.ead.project.dreamer.domain.servers
 
 import android.content.Context
 import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.presentation.server.menu.MenuServerFragment
 import javax.inject.Inject
 
 class HandleChapter @Inject constructor(
-    private val launchServer: LaunchServer,
     private val launchVideo: LaunchVideo
 ) {
 
     operator fun invoke(context: Context, chapter: Chapter) {
         if (chapter.isDownloaded()) {
-            launchVideo(context, chapter,true)
+            launchVideo(context, chapter, null,true)
         }
         else {
-            launchServer(context, chapter,false)
+            MenuServerFragment.launch(context,chapter,false)
         }
     }
 }
