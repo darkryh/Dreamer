7bea13489a9e96f24a2f2aaa41b9babed3384d73
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:32:44 -0500

    added configure function to pass json objects in the manager

--- app/src/main/java/com/ead/project/dreamer/app/data/util/system/DownloadManagerExtensions.kt
+++ app/src/main/java/com/ead/project/dreamer/app/data/util/system/DownloadManagerExtensions.kt
@@ -2,6 +2,11 @@ package com.ead.project.dreamer.app.data.util.system
 
 import android.app.DownloadManager
 import android.database.Cursor
+import android.os.Environment
+import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.data.models.Download
+import com.ead.project.dreamer.data.models.Update
+import com.google.gson.Gson
 
 
 fun DownloadManager.resetDownloads() {
@@ -13,4 +18,26 @@ fun DownloadManager.resetDownloads() {
 
 fun DownloadManager.getAllQuery() : Cursor {
     return query(DownloadManager.Query())
+}
+
+fun DownloadManager.Request.configure(gson: Gson, chapter: Chapter, subPath: String) : DownloadManager.Request {
+    return this
+        .setDescription(
+            gson.toJson(
+                Download(0,chapter.id,chapter.title,chapter.number, Download.DOWNLOAD_TYPE_CHAPTER,0,0,0)
+            )
+        )
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,subPath)
+}
+
+fun DownloadManager.Request.configure(gson: Gson, update: Update, subPath: String) : DownloadManager.Request {
+    return this
+        .setDescription(
+            gson.toJson(
+                Download(0,-1,"${update.title}_${update.version}",0, Download.DOWNLOAD_TYPE_UPDATE,0,0,0)
+            )
+        )
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,subPath)
 }
