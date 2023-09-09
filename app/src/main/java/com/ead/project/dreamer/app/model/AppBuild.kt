749962ca5e5965dda595107efd9128a84231fdc4
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 21:49:52 -0500

    data class to handle with the app states, including update cases.

--- app/src/main/java/com/ead/project/dreamer/app/model/AppBuild.kt
+++ app/src/main/java/com/ead/project/dreamer/app/model/AppBuild.kt
@@ -1,12 +1,17 @@
 package com.ead.project.dreamer.app.model
 
+import android.os.Parcelable
+import com.ead.project.dreamer.data.models.Update
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class AppBuild(
     val minVersion: Double,
-    val lastVersion : Double,
+    val update : Update,
     val resumedVersionNotes : String?,
     val versionNotes : String?,
     val downloadReference : String,
     val currentVersionDeprecated : Boolean = false,
     val isUnlockedVersion : Boolean = true,
     val isDarkTheme : Boolean = false
-)
+) : Parcelable
