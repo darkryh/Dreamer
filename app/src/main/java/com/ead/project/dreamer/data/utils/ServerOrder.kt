149834c25539173517c01a31f8cd23735de2a447
darkryh <alex_torres-xc@hotmail.com>
2023-09-09 00:26:35 -0500

    class that hold server automatic preferences by default

---
+++ app/src/main/java/com/ead/project/dreamer/data/utils/ServerOrder.kt
@@ -0,0 +1,84 @@
+package com.ead.project.dreamer.data.utils
+
+import com.ead.project.dreamer.app.data.server.Server
+
+object ServerOrder {
+
+    val internalServers : List<String> = listOf(
+        Server.ONEFICHIER,
+        Server.OKRU,
+        Server.PIXELDRAIN,
+        Server.MEDIAFIRE,
+        Server.STREAMSB,
+        Server.SENDVID,
+        Server.VIDEOBIN,
+        Server.FEMBED,
+        Server.VOE,
+        Server.BAYFILES,
+        Server.ZIPPYSHARE,
+        Server.STREAMTAPE,
+        Server.SOLIDFILES,
+        Server.PUJ,
+        Server.GOOGLE_DRIVE,
+        Server.ANONFILE,
+        Server.FIRELOAD,
+        Server.MP4UPLOAD,
+        Server.FILEMOON,
+        Server.STREAMWISH,
+        Server.MIXDROP,
+        Server.UQLOAD,
+        Server.MEGA
+    )
+
+    val externalServers : List<String> = listOf(
+        Server.OKRU,
+        Server.ONEFICHIER,
+        Server.MEDIAFIRE,
+        Server.PIXELDRAIN,
+        Server.STREAMSB,
+        Server.SENDVID,
+        Server.VIDEOBIN,
+        Server.FEMBED,
+        Server.VOE,
+        Server.ZIPPYSHARE,
+        Server.BAYFILES,
+        Server.STREAMTAPE,
+        Server.SOLIDFILES,
+        Server.PUJ,
+        Server.GOOGLE_DRIVE,
+        Server.ANONFILE,
+        Server.FIRELOAD,
+        Server.MP4UPLOAD,
+        Server.FILEMOON,
+        Server.STREAMWISH,
+        Server.MIXDROP,
+        Server.UQLOAD,
+        Server.MEGA
+    )
+
+    val downloadServers : List<String> = listOf(
+        Server.ONEFICHIER,
+        Server.MEDIAFIRE,
+        Server.PIXELDRAIN,
+        Server.OKRU,
+        Server.FEMBED,
+        Server.SENDVID,
+        Server.VOE,
+        Server.ZIPPYSHARE,
+        Server.BAYFILES,
+        Server.STREAMTAPE,
+        Server.SOLIDFILES,
+        Server.PUJ,
+        Server.VIDEOBIN,
+        Server.STREAMSB,
+        Server.GOOGLE_DRIVE,
+        Server.ANONFILE,
+        Server.FIRELOAD,
+        Server.MP4UPLOAD,
+        Server.FILEMOON,
+        Server.STREAMWISH,
+        Server.MIXDROP,
+        Server.UQLOAD,
+        Server.MEGA
+    )
+}
