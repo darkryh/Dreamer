bafd6191181834af13c361317891e4bd54976b47
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:11:24 -0500

    deleted some unused functions

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/DownloadEngine.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/DownloadEngine.kt
@@ -1,138 +1,136 @@
 package com.ead.project.dreamer.domain.downloads
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.IntentFilter
 import android.graphics.Bitmap
 import android.webkit.WebView
+import com.ead.project.dreamer.app.AppReceiver
+import com.ead.project.dreamer.app.data.downloads.DownloadStore
+import com.ead.project.dreamer.app.data.downloads.util.DownloadWebView
 import com.ead.project.dreamer.app.data.util.system.load
-import com.ead.project.dreamer.app.data.util.system.onDestroy
 import com.ead.project.dreamer.data.AnimeRepository
 import com.ead.project.dreamer.data.database.model.Chapter
 import com.ead.project.dreamer.data.models.Server
 import com.ead.project.dreamer.data.network.DreamerClient
-import com.ead.project.dreamer.data.network.DreamerWebView
+import com.ead.project.dreamer.data.utils.Run
 import com.ead.project.dreamer.data.utils.Thread
-import com.ead.project.dreamer.app.data.downloads.DownloadsReceiver
+import com.ead.project.dreamer.domain.databasequeries.GetChapter
 import com.ead.project.dreamer.domain.servers.GetServerResultToArray
 import com.ead.project.dreamer.domain.servers.GetServers
 import com.ead.project.dreamer.domain.servers.GetSortedServers
 import com.ead.project.dreamer.domain.servers.ServerScript
-import kotlinx.coroutines.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DownloadEngine @Inject constructor(
     private val context: Context,
     private val repository: AnimeRepository,
-    private val tempDownloads: GetTempDownloads,
+    private val getChapter: GetChapter,
     private val getServerResultToArray: GetServerResultToArray,
     private val getSortedServers: GetSortedServers,
     private val getServers: GetServers,
-    private val createDownload: CreateDownload,
-    private val serverScript: ServerScript
+    private val enqueueDownload: EnqueueDownload,
+    private val serverScript: ServerScript,
+    private val downloadStore: DownloadStore,
+    private val appReceiver: AppReceiver
 ) {
-    private var webView : DreamerWebView?= null
+
+    private lateinit var webView: DownloadWebView
     private var chapter : Chapter?= null
 
-    private var executor: ExecutorService = Executors.newSingleThreadExecutor()
     private val scope = CoroutineScope(Dispatchers.IO)
-    private var broadcastReceiver : BroadcastReceiver?= null
+
+    init {
+        Thread.onUi {
+            webView = DownloadWebView(context)
+            initWebView()
+        }
+    }
 
     operator fun invoke() {
-        this.chapter = tempDownloads.getChapter()
-        settingBroadcast()
-        runOnUI {
-            if (chapter != null) loadChapter()
-            else onCompleted()
+        this.chapter = getEnqueuedChapter()?:return
+
+        if (chapter != null) {
+            loadChapter()
+        }
+        else {
+            onCompleted()
         }
     }
 
-    fun isNotWorking() = webView == null
-
-    private fun loadChapter() = getWebView().load(chapter!!.reference)
-
-    private fun getWebView() : DreamerWebView = webView?:DreamerWebView(context).also {
-        webView = it
-        settingWebView()
+    private fun getEnqueuedChapter() : Chapter? = runBlocking {
+        getChapter.fromId(downloadStore.getEnqueuedIdChapter()?:-1)
     }
 
-    fun settingBroadcast() {
-        if (broadcastReceiver == null) {
-            broadcastReceiver = DownloadsReceiver()
-            registerReceiver()
-        }
-    }
+    private fun loadChapter() = webView.load(chapter?.reference?:"null")
 
-    private fun settingWebView() {
-        webView?.webViewClient = object : DreamerClient() {
+    private fun initWebView() {
+        webView.webViewClient = object : DreamerClient() {
             override fun onTimeout(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onTimeout(view, url, favicon)
-                executor.execute {
-                    repository.updateChapterNormal(chapter?.copy(
-                    state = Chapter.STATUS_FAILED)?:return@execute)
+                scope.launch {
+                    repository.updateChapter(
+                        chapter?.copy(state = Chapter.STATUS_FAILED)?:return@launch
+                    )
                 }
-                invoke()
             }
 
             override fun onPageLoaded(view: WebView?, url: String?) {
                 super.onPageLoaded(view, url)
-                try {
-                    webView?.evaluateJavascript(serverScript()) {
+                Run.catching {
+                    webView.evaluateJavascript(serverScript()) {
                         getServers(getSortedServers(getServerResultToArray(it),true))
                     }
-                } catch (e : Exception) { e.printStackTrace() }
+                }
             }
-
         }
     }
 
     private fun getServers(it: List<String>) = scope.launch (Dispatchers.IO) {
-        prepareDownload(withContext(Dispatchers.Default) { getServers.fromCoroutine(it) }) }
+        prepareDownload(withContext(Dispatchers.Default) {
+            getServers.fromCoroutine(it)
+        })
+    }
 
     private fun prepareDownload(serverList: List<Server>) {
-        try {
+        Run.catching {
             var isDownloadFailed = true
+
             for (server in serverList) {
                 if (server.isValidated) {
-                    launchDownload(server.videoList.last().directLink)
+
+                    enqueueDownload(server.videoList.last().directLink)
                     isDownloadFailed = false
                     break
+
                 }
                 else if (server.isConnectionValidated) {
-                    launchDownload(server.videoList.last().directLink)
+
+                    enqueueDownload(server.videoList.last().directLink)
                     isDownloadFailed = false
                     break
+
                 }
             }
-            if (isDownloadFailed) invoke()
+
+            if (isDownloadFailed) restart()
         }
-        catch ( e : InterruptedException) { invoke() }
     }
 
-    private fun launchDownload(url : String) =
-        runOnUI {
-            createDownload(chapter,url)
-            invoke()
-        }
+    private fun enqueueDownload(url : String) {
+        enqueueDownload(chapter,url)
+        restart()
+    }
+
+    fun restart() = invoke()
+
+    fun isLoading() : Boolean = (webView.webViewClient  as DreamerClient).isLoading
 
     private fun onCompleted() {
-        unregisterReceiver()
-        onDestroy()
+        AppReceiver.unregister(context,appReceiver)
     }
 
-    private fun registerReceiver() = try { context.registerReceiver(broadcastReceiver,
-            IntentFilter(android.app.DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-    } catch (e : IllegalArgumentException) { e.printStackTrace() }
-
-    private fun unregisterReceiver() = try { context.unregisterReceiver(broadcastReceiver) }
-    catch (e : IllegalArgumentException) { e.printStackTrace()}
-
-    private fun onDestroy() {
-        webView?.onDestroy()
-        webView = null
-    }
-
-    private fun runOnUI(task: () -> Unit) = Thread.onUi { task() }
 }
