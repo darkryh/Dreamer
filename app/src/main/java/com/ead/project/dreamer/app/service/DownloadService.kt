03a1c6e12f0e4ab45526299717a796fc173e88ed
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:33:37 -0500

    service to handle automatic downloads, it i´ll work soon

---
+++ app/src/main/java/com/ead/project/dreamer/app/service/DownloadService.kt
@@ -0,0 +1,42 @@
+package com.ead.project.dreamer.app.service
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DownloadService : Service() {
+
+
+    companion object {
+        private var isRunning = false
+
+        fun isRunning() : Boolean = isRunning
+
+        fun setIsRunning(value : Boolean) {
+            isRunning = value
+        }
+
+        fun start(context : Context) {
+            if (isRunning) return
+        }
+    }
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        setIsRunning(true)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val action = intent?.action
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        setIsRunning(false)
+        super.onDestroy()
+    }
+}
