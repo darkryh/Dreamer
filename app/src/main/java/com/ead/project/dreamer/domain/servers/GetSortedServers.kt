7589ccc331b03f373ee28a9ef5cdcbacde51475a
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:59:53 -0500

    optimized code using persistence.

--- app/src/main/java/com/ead/project/dreamer/domain/servers/GetSortedServers.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/servers/GetSortedServers.kt
@@ -10,109 +10,34 @@ class GetSortedServers @Inject constructor(
 ) {
 
     private val playerPreferences = preferenceUseCase.playerPreferences
+    private var serversOrder : List<String> = emptyList()
 
     operator fun invoke(embedList : List<String>, isDownload : Boolean) : MutableList<String> {
-        val sortedList : MutableList<Pair<Int,String>> = ArrayList()
+        val servers : MutableList<Pair<Int,String>> = ArrayList()
 
-        if (!isDownload) {
-            if (!playerPreferences.isInExternalMode()) {
-
-                for (embedUrl in embedList) {
-                    sortedList.add(Pair(getPositions(embedUrl),embedUrl))
-                }
-            }
-            else {
-                for (embedUrl in embedList) {
-                    sortedList.add(Pair(getPositionsExternal(embedUrl),embedUrl))
-                }
-            }
+        serversOrder = if (!isDownload) {
+            if (!playerPreferences.isInExternalMode())
+                Server.getSortedInternalServers()
+            else
+                Server.getSortedExternalServers()
         }
-        else {
+        else Server.getSortedDownloadServers()
 
-            for (embedUrl in embedList) {
-                sortedList.add(Pair(getDownloadsPosition(embedUrl),embedUrl))
-            }
+        for (embedUrl in embedList) servers.add(Pair(getIndexed(embedUrl),embedUrl))
+        
+        return servers.sorted()
+    }
 
+    private fun getIndexed(data : String) : Int {
+        serversOrder.forEachIndexed { index, server ->
+            if (serverIdentifier(data) == server)
+                return index
         }
-
-        return sortedList
-            .apply { sortBy { it.first } }
+        return 10000
+    }
+    
+    private fun MutableList<Pair<Int,String>>.sorted() : MutableList<String> =
+        this.apply { sortBy { it.first } }
             .map { it.second }
             .toMutableList()
-    }
-
-    private fun getPositions(data : String) : Int {
-        return when (serverIdentifier(data)) {
-            Server.MEDIAFIRE -> 0
-            Server.OKRU -> 1
-            Server.ONEFICHIER -> 2
-            Server.STREAMSB -> 3
-            Server.SENDVID -> 4
-            Server.VIDEOBIN -> 5
-            Server.FEMBED -> 6
-            Server.VOE -> 7
-            Server.BAYFILES -> 8
-            Server.ZIPPYSHARE -> 9
-            Server.STREAMTAPE -> 10
-            Server.SOLIDFILES -> 11
-            Server.PUJ -> 12
-            Server.GOOGLE_DRIVE -> 13
-            Server.ANONFILE -> 14
-            Server.FIRELOAD -> 15
-            Server.MP4UPLOAD -> 16
-            Server.UQLOAD -> 17
-            Server.MEGA -> 18
-            else -> 10000
-        }
-    }
-
-    private fun getPositionsExternal(data : String) : Int {
-        return when (serverIdentifier(data)) {
-            Server.MEDIAFIRE -> 0
-            Server.ONEFICHIER -> 1
-            Server.FEMBED -> 2
-            Server.STREAMSB -> 3
-            Server.SENDVID -> 4
-            Server.VIDEOBIN -> 5
-            Server.VOE -> 6
-            Server.OKRU -> 7
-            Server.ZIPPYSHARE -> 8
-            Server.BAYFILES -> 9
-            Server.STREAMTAPE -> 10
-            Server.SOLIDFILES -> 11
-            Server.PUJ -> 12
-            Server.GOOGLE_DRIVE -> 13
-            Server.ANONFILE -> 14
-            Server.FIRELOAD -> 15
-            Server.MP4UPLOAD -> 16
-            Server.UQLOAD -> 17
-            Server.MEGA -> 18
-            else -> 10000
-        }
-    }
-
-    private fun getDownloadsPosition(data: String) : Int {
-        return when (serverIdentifier(data)) {
-            Server.ONEFICHIER -> 0
-            Server.MEDIAFIRE -> 1
-            Server.OKRU -> 2
-            Server.FEMBED -> 3
-            Server.SENDVID -> 4
-            Server.VOE -> 5
-            Server.ZIPPYSHARE -> 6
-            Server.BAYFILES -> 7
-            Server.STREAMTAPE -> 8
-            Server.SOLIDFILES -> 9
-            Server.PUJ -> 10
-            Server.VIDEOBIN -> 11
-            Server.STREAMSB -> 12
-            Server.GOOGLE_DRIVE -> 13
-            Server.ANONFILE -> 14
-            Server.FIRELOAD -> 15
-            Server.MP4UPLOAD -> 16
-            Server.UQLOAD -> 17
-            Server.MEGA -> 18
-            else -> 10000
-        }
-    }
 }
