d263c3d6d757bbe2ae697f01878ba11ab6f3c68f
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:48:01 -0500

    EnqueueDownload to store download in case to store and download manager

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/EnqueueDownload.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/EnqueueDownload.kt
@@ -1,13 +1,32 @@
 package com.ead.project.dreamer.domain.downloads
 
-import com.ead.project.dreamer.app.data.downloads.DownloadStore
+import android.app.DownloadManager
+import android.content.Context
+import android.content.IntentFilter
+import com.ead.project.dreamer.app.AppReceiver
+import com.ead.project.dreamer.app.data.util.Apk
+import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.data.models.Update
 import javax.inject.Inject
 
 class EnqueueDownload @Inject constructor(
-    private val downloadStore: DownloadStore
+    private val generateDownload: GenerateDownload,
+    private val configureDownload: ConfigureDownload,
+    private val appReceiver: AppReceiver,
+    private val context: Context
 ) {
 
-    operator fun invoke(downloadId : Long, chapterId : Int) {
-        downloadStore.addDownload(downloadId, chapterId)
+    operator fun invoke(chapter : Chapter?,url : String) : Long {
+        registerReceiver(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        return generateDownload(configureDownload(chapter?:return -1,url),chapter.id)
+    }
+
+    operator fun invoke(update : Update, url: String) : Long {
+        return generateDownload(configureDownload(update, url))
+    }
+
+    private fun registerReceiver(action : String) {
+        val intentFilter = IntentFilter(action)
+        AppReceiver.register(context,appReceiver,intentFilter)
     }
 }
