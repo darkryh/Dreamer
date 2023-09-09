45c16d98cbe41db5905849e38d1cfacfc12feed1
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:07:09 -0500

    launch download class for automatic case.

--- app/src/main/java/com/ead/project/dreamer/domain/downloads/LaunchDownload.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/LaunchDownload.kt
@@ -1,26 +1,28 @@
 package com.ead.project.dreamer.domain.downloads
 
-import android.content.Context
-import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import androidx.work.Data
+import androidx.work.ExistingPeriodicWorkPolicy
+import com.ead.project.dreamer.app.data.worker.Worker
 import com.ead.project.dreamer.data.database.model.Chapter
-import com.ead.project.dreamer.presentation.chapter.settings.ChapterSettingsFragment
+import com.ead.project.dreamer.domain.configurations.LaunchPeriodicTimeRequest
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class LaunchDownload @Inject constructor() {
+class LaunchDownload @Inject constructor(val launchPeriodicTimeRequest: LaunchPeriodicTimeRequest) {
+    operator fun invoke(chapter: Chapter) {
 
-    private val bundle = Bundle()
+        val data: Data = Data.Builder()
+            .putInt(Worker.DOWNLOAD_CHAPTER_KEY,chapter.id)
+            .build()
 
-    operator fun invoke(context: Context, chapter: Chapter,isChapter : Boolean) {
-        val fragmentManager = (context as FragmentActivity).supportFragmentManager
-        val chapterSettings = ChapterSettingsFragment()
-        chapterSettings.apply {
-            arguments = bundle.apply {
-                clear()
-                putParcelable(Chapter.REQUESTED, chapter)
-                putBoolean(ChapterSettingsFragment.IS_INSTANCE_A_CHAPTER,isChapter)
-            }
-            show(fragmentManager, ChapterSettingsFragment.FRAGMENT)
-        }
+
+        launchPeriodicTimeRequest(
+            LaunchPeriodicTimeRequest.DownloadWorker,
+            5,
+            TimeUnit.MINUTES,
+            Worker.SYNC_DOWNLOADS,
+            ExistingPeriodicWorkPolicy.UPDATE,
+            data
+        )
     }
 }
