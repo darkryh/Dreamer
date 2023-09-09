9b3e3b3cd7088b0cc690a9e691e7ab9a48ae54a3
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 22:11:15 -0500

    removed, moved, or renamed classes in most of the cases unused.

--- app/src/main/java/com/ead/project/dreamer/app/data/util/system/ProgressBarExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.ead.project.dreamer.app.data.util.system
-
-import android.view.Gravity
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-
-fun ProgressBar.centerGravity() {
-    (layoutParams as LinearLayout.LayoutParams).gravity = Gravity.CENTER
-}
