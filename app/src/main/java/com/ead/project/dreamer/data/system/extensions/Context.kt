dee23e34f1abd956d659c44208f3594e981e05a7
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:13:09 -0500

    add logs options in context

--- app/src/main/java/com/ead/project/dreamer/data/system/extensions/Context.kt
+++ app/src/main/java/com/ead/project/dreamer/data/system/extensions/Context.kt
@@ -1,6 +1,7 @@
 package com.ead.project.dreamer.data.system.extensions
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
@@ -8,6 +9,17 @@ fun Context.toast(text : String,duration : Int = Toast.LENGTH_LONG) {
     Toast.makeText(this,text,duration).show()
 }
 
+fun Context.debug(text: String) {
+    Log.d(DEBUG, text)
+}
+
+fun Context.error(text: String) {
+    Log.e(ERROR, text)
+}
+
 fun Fragment.toast(text : String,duration : Int = Toast.LENGTH_LONG) {
     requireActivity().toast(text, duration)
 }
+
+const val DEBUG = "debug"
+const val ERROR = "error"
