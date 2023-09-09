84bde437050612fbce0dfed7fb765ad1d2de6d7b
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:50:16 -0500

    renamed some files preferences and pathing

--- app/src/main/java/com/ead/project/dreamer/app/data/files/Files.kt
+++ app/src/main/java/com/ead/project/dreamer/app/data/files/Files.kt
@@ -2,16 +2,20 @@ package com.ead.project.dreamer.app.data.files
 
 import android.os.Environment
 import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.data.models.Update
 import java.io.File
 
 object Files {
 
-    private const val MAIN = "Mc files"
+    private const val MAIN = "Dreamer Files"
     private const val SERIES = "series"
+    private const val UPDATES = "updates"
+    private const val CHAPTER = "Capítulo"
     private val DIRECTORY_DOWNLOADS : String = Environment.DIRECTORY_DOWNLOADS
 
-    val mainFile = getMainFolder()
-    val seriesFile = File(mainFile, SERIES)
+    val mainFile get() = getMainFolder()
+    val seriesFile get() = File(mainFile, SERIES)
+    val updatesFile get() = File(mainFile, UPDATES)
 
     val DirectoryDownloadsFile : File =
         Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS)
@@ -21,8 +25,19 @@ object Files {
     }
 
     fun getChapterRoute(chapter: Chapter) : String {
-        return seriesFile.absolutePath + "/" + chapter.title + "/" + chapter.title +
-                " Capítulo ${chapter.number}" +".mp4"
+        return seriesFile.absolutePath + "/" + chapter.title + "/" + chapter.title + " $CHAPTER ${chapter.number}" +".mp4"
+    }
+
+    fun getChapterSubPath(chapter: Chapter) : String {
+        return  mainFile.name + "/" + seriesFile.name + "/"  + chapter.title + "/" + chapter.title + " $CHAPTER ${chapter.number}" +".mp4"
+    }
+
+    fun getUpdateRoute(update: Update) : String {
+        return updatesFile.absolutePath + "/" + update.title + "_" + update.version + ".apk"
+    }
+
+    fun getUpdateSubPath(update: Update) : String {
+        return  mainFile.name + "/" + updatesFile.name + "/" + update.title + "_" + update.version + ".apk"
     }
 
     private fun getMainFolder() : File {
