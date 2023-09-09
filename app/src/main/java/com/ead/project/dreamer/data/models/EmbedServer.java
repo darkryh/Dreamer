d0343f82e97f7d3179edbbb08510c99b0563d88c
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:09:55 -0500

    Main class that holds, similar initialization

---
+++ app/src/main/java/com/ead/project/dreamer/data/models/EmbedServer.java
@@ -0,0 +1,30 @@
+package com.ead.project.dreamer.data.models;
+
+import javax.inject.Inject;
+
+public class EmbedServer extends Server {
+
+    @Inject
+    public EmbedServer(String embeddedUrl,Player player) {
+        super(embeddedUrl);
+
+        if (isDownloading) return;
+
+        this.player = player;
+        this.isDirect = false;
+
+        this.url = setupEmbeddedUrl(embeddedUrl);
+
+        if (checkIfVideoIsAvailable()) {
+            addDefaultVideo();
+        }
+    }
+
+    protected String setupEmbeddedUrl(String embeddedUrl) {
+        return embeddedUrl;
+    }
+
+    protected Boolean checkIfVideoIsAvailable() {
+        return false;
+    }
+}
