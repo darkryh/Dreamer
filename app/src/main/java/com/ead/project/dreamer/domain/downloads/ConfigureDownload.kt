de2ac03b6e35725d33f07d0b21f9397124b5f515
darkryh <alex_torres-xc@hotmail.com>
2023-09-08 23:12:06 -0500

    renewed class that configure json that passes into the notifications from DownloadManager.

---
+++ app/src/main/java/com/ead/project/dreamer/domain/downloads/ConfigureDownload.kt
@@ -0,0 +1,44 @@
+package com.ead.project.dreamer.domain.downloads
+
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import com.ead.project.dreamer.R
+import com.ead.project.dreamer.app.data.util.system.configure
+import com.ead.project.dreamer.app.data.util.system.configureFolder
+import com.ead.project.dreamer.data.database.model.Chapter
+import com.ead.project.dreamer.data.models.Update
+import com.ead.project.dreamer.domain.PreferenceUseCase
+import com.google.gson.Gson
+import java.io.File
+import javax.inject.Inject
+
+class ConfigureDownload @Inject constructor(
+    private val context: Context,
+    private val gson: Gson,
+    preferenceUseCase: PreferenceUseCase
+) {
+
+    private val filesPreferences = preferenceUseCase.filesPreferences
+
+    operator fun invoke (chapter: Chapter, url : String) : DownloadManager.Request {
+
+        val chapterDirectory = File(filesPreferences.series.absolutePath, chapter.title)
+        chapterDirectory.configureFolder()
+
+        return DownloadManager.Request(Uri.parse(url))
+            .setTitle(context.getString(R.string.title_and_number_short,chapter.title,chapter.number.toString()))
+            .configure(gson,chapter,filesPreferences.getChapterSubPath(chapter))
+    }
+
+    operator fun invoke(update: Update, url: String) : DownloadManager.Request {
+
+        val updateDirectory = filesPreferences.updates
+        updateDirectory.configureFolder()
+
+        return DownloadManager.Request(Uri.parse(url))
+            .setTitle(context.getString(R.string.title_and_version,update.title,update.version.toString()))
+            .configure(gson,update,filesPreferences.getUpdateSubPath(update))
+    }
+
+}
