13e4a844f0c067177d58be11265a7e744dde47ff
darkryh <alex_torres-xc@hotmail.com>
2023-10-23 21:35:08 -0500

    updated StreamWish server from web to normal server

--- app/src/main/java/com/ead/project/dreamer/data/models/server/StreamWish.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/server/StreamWish.kt
@@ -1,11 +1,27 @@
 package com.ead.project.dreamer.data.models.server
 
-import com.ead.project.dreamer.data.models.EmbedServer
-import com.ead.project.dreamer.data.models.Player
+import com.ead.project.dreamer.data.models.Server
+import com.ead.project.dreamer.data.utils.PatternManager
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class StreamWish(embeddedUrl:String) : EmbedServer(embeddedUrl,Player.StreamWish) {
+class StreamWish(embeddedUrl:String) : Server(embeddedUrl) {
 
-    override fun checkIfVideoIsAvailable(): Boolean {
-        return !super.checkIfVideoIsAvailable()
+    override fun onExtract() {
+        try {
+            val response = OkHttpClient()
+                .newCall(Request.Builder().url(url).build())
+                .execute()
+
+            url = PatternManager.singleMatch(
+                response.body?.string().toString(),
+                """file:"(https://[^"]+)"""",
+                1
+            )
+            if (url != null) addDefaultVideo()
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+        }
     }
 }
