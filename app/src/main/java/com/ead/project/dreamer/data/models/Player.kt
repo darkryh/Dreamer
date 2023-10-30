e46add404be98a10136c709ceb669f94152a83c7
darkryh <alex_torres-xc@hotmail.com>
2023-10-30 13:55:30 -0500

    adOrder class to handle combining items with anyList livedata and nextChapterFrom function.

--- app/src/main/java/com/ead/project/dreamer/data/models/Player.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/Player.kt
@@ -3,5 +3,5 @@ package com.ead.project.dreamer.data.models
 enum class Player {
     Bayfiles,StreamSb,Fembed,Fireload,Mega,MegaUp,Mp4Upload, Okru,Onefichier,Puj,Senvid,SolidFiles,
     Streamtape,Uptobox, Uqload,Videobin,Zippyshare,Voe,Anonfile,DoodStream,Vidlox,YourUpload,
-    StreamWish,MixDrop,FileMoon,PixelDrain,Blank
+    StreamWish,MixDrop,FileMoon,Filelions,GoogleDrive,Mediafire,VidGuard,PixelDrain,Blank
 }
--- app/src/main/java/com/ead/project/dreamer/presentation/player/PlayerViewModel.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/player/PlayerViewModel.kt
@@ -1,12 +1,18 @@
 package com.ead.project.dreamer.presentation.player
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ead.project.dreamer.app.data.player.casting.CastManager
 import com.ead.project.dreamer.data.database.model.AnimeProfile
 import com.ead.project.dreamer.data.database.model.Chapter
-import com.ead.project.dreamer.app.data.player.casting.CastManager
-import com.ead.project.dreamer.data.utils.AdManager
-import com.ead.project.dreamer.domain.*
+import com.ead.project.dreamer.data.utils.AdOrder
+import com.ead.project.dreamer.domain.ChapterUseCase
+import com.ead.project.dreamer.domain.ObjectUseCase
+import com.ead.project.dreamer.domain.PreferenceUseCase
+import com.ead.project.dreamer.domain.ProfileUseCase
 import com.ead.project.dreamer.domain.servers.HandleChapter
 import com.google.android.gms.cast.framework.CastContext
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -23,13 +29,30 @@ class PlayerViewModel @Inject constructor(
     val handleChapter: HandleChapter,
     val castContext: CastContext,
     val castManager: CastManager,
-    val adManager: AdManager,
     application: Application
 ): AndroidViewModel(application) {
 
     val preferences = preferenceUseCase.preferences
     val playerPreferences = preferenceUseCase.playerPreferences
     private val adPreferences = preferenceUseCase.adPreferences
+
+    private val adOrder by lazy {
+        AdOrder(
+            items = mutableListOf(),
+            ads = emptyList()
+        )
+    }
+
+    private val _nextChapter : MutableLiveData<Chapter?> = MutableLiveData(null)
+
+    private val _recommendedProfiles : MutableLiveData<List<Any>> = MutableLiveData()
+    val recommendedProfiles : LiveData<List<Any>> = _recommendedProfiles
+
+    fun setRecommendedProfiles(list : List<Any>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            adOrder.setup(list,_recommendedProfiles)
+        }
+    }
 
     fun addViewedTime() {
         adPreferences.addViewedTime()
@@ -41,6 +64,14 @@ class PlayerViewModel @Inject constructor(
     fun getProfileData(id: Int) : LiveData<AnimeProfile?> = profileUseCase.getProfile.livedata(id)
 
     fun getChaptersFromProfile (id : Int) = chapterUseCase.getChapters.livedata(id,false)
+
+    fun geNextChapterFrom(chapter: Chapter) : LiveData<Chapter?> {
+        viewModelScope.launch(Dispatchers.IO) {
+            val nextChapter = chapterUseCase.getChapter.fromTitleAndNumber(chapter.title,chapter.number + 1)
+            _nextChapter.postValue(nextChapter)
+        }
+        return _nextChapter
+    }
 
     fun getProfilesListFrom(animeProfile: AnimeProfile) : LiveData<List<AnimeProfile>> =
         profileUseCase.getProfilePlayerRecommendations.livedata(animeProfile)
