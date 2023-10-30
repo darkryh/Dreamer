70e661a5ec71560d1305804fa940c05b5e2deffc
darkryh <alex_torres-xc@hotmail.com>
2023-10-30 13:51:39 -0500

    override constructor server

--- app/src/main/java/com/ead/project/dreamer/data/models/server/NullServer.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/server/NullServer.kt
@@ -3,7 +3,4 @@ package com.ead.project.dreamer.data.models.server
 import com.ead.project.dreamer.data.models.Player
 import com.ead.project.dreamer.data.models.Server
 
-class NullServer (embeddedUrl:String) : Server(embeddedUrl) {
-
-    override fun onPreExtract() { this.player = Player.Blank }
-}
+class NullServer (embeddedUrl:String) : Server(embeddedUrl,Player.Blank)
