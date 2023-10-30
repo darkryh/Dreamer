442a942756ff704e25c4342272762ba282ab51b3
darkryh <alex_torres-xc@hotmail.com>
2023-10-30 02:23:57 -0500

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KaptGenerateStubs
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
-}
-
-allprojects {
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    tasks.withType<KaptGenerateStubs>().configureEach {
-        kotlinOptions.jvmTarget = "1.8"
-    }
 }
