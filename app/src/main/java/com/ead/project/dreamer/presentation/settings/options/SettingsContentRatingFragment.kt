fcda5c940427da51c13b5ff3b029f0e3fea69aca
darkryh <alex_torres-xc@hotmail.com>
2023-10-30 13:39:29 -0500

    formatted functionality from google policy to unlocked version

--- app/src/main/java/com/ead/project/dreamer/presentation/settings/options/SettingsContentRatingFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/settings/options/SettingsContentRatingFragment.kt
@@ -8,6 +8,8 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import com.ead.project.dreamer.R
 import com.ead.project.dreamer.app.AppInfo
+import com.ead.project.dreamer.data.system.extensions.toast
+import com.ead.project.dreamer.data.utils.Thread
 import com.ead.project.dreamer.presentation.settings.viewmodels.SettingsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -18,7 +20,7 @@ class SettingsContentRatingFragment : PreferenceFragmentCompat() {
 
     private val viewModel : SettingsViewModel by viewModels()
 
-    private lateinit var swPrivacyPolicy : SwitchPreference
+    private lateinit var swFullContent : SwitchPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.content_rating_preferences, rootKey)
@@ -27,21 +29,25 @@ class SettingsContentRatingFragment : PreferenceFragmentCompat() {
     }
 
     private fun initLayouts() {
-        swPrivacyPolicy = findPreference(PREFERENCE_GOOGLE_POLICY)?:return
+        swFullContent = findPreference(PREFERENCE_FULL_CONTENT)?:return
     }
 
     private fun settingLayouts() {
         lifecycleScope.launch {
-            viewModel.isGoogleVersion.collectLatest { isGooglePolicyActivated  ->
-                swPrivacyPolicy.isChecked = isGooglePolicyActivated
+            viewModel.isUnlockedVersion.collectLatest { isUnlockedVersion  ->
+                swFullContent.isChecked = isUnlockedVersion
             }
         }
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
         return when (val key = preference.key) {
-            PREFERENCE_GOOGLE_POLICY -> {
+            PREFERENCE_FULL_CONTENT -> {
                 return if (AppInfo.isGoogleAppVersion) {
+                    Thread.onClickEffect {
+                        swFullContent.isChecked = false
+                        toast(getString(R.string.google_policies_mode))
+                    }
                     viewModel.getBooleanKeyPreference(key)
                 }
                 else {
@@ -53,6 +59,6 @@ class SettingsContentRatingFragment : PreferenceFragmentCompat() {
     }
 
     companion object {
-        const val PREFERENCE_GOOGLE_POLICY = "PREFERENCE_GOOGLE_POLICY"
+        const val PREFERENCE_FULL_CONTENT = "PREFERENCE_FULL_CONTENT"
     }
 }
