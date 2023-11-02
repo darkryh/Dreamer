fe11106dd0f54b231b015740516396a88d75523e
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:41:39 -0500

    handed on layout preferences

--- app/src/main/java/com/ead/project/dreamer/presentation/settings/options/SettingsAccountFragment.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/settings/options/SettingsAccountFragment.kt
@@ -4,16 +4,22 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.ead.commons.lib.views.setResourceImageAndColor
 import com.ead.project.dreamer.R
-import com.ead.project.dreamer.app.data.discord.Discord
 import com.ead.project.dreamer.app.data.util.system.launchActivityAndFinish
+import com.ead.project.dreamer.app.model.EadAccount
 import com.ead.project.dreamer.presentation.login.LoginActivity
 import com.ead.project.dreamer.presentation.settings.layouts.AccountViewPreference
 import com.ead.project.dreamer.presentation.settings.viewmodels.SettingsViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SettingsAccountFragment : PreferenceFragmentCompat() {
@@ -22,9 +28,9 @@ class SettingsAccountFragment : PreferenceFragmentCompat() {
 
     private lateinit var prefAdviser : Preference
     private lateinit var prefSession : Preference
-    private var avAccountPreference: AccountViewPreference? = null
-    private val discordUser = Discord.getUser()
+    private lateinit var accountPreference: AccountViewPreference
 
+    private var eadAccount : EadAccount?=null
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.account_preferences, rootKey)
         initLayouts()
@@ -32,29 +38,46 @@ class SettingsAccountFragment : PreferenceFragmentCompat() {
     }
 
     private fun initLayouts() {
-        avAccountPreference = findPreference(PREFERENCE_CUSTOMIZED_IMV_PROFILE) as AccountViewPreference?
+        accountPreference = findPreference(PREFERENCE_CUSTOMIZED_IMV_PROFILE)?:return
         prefAdviser = findPreference(PREFERENCE_ADVISER)?:return
         prefSession = findPreference(PREFERENCE_SESSION)?:return
     }
 
     private fun userConfiguration() {
-        if (discordUser != null) {
-            prefAdviser.isVisible = false
+        lifecycleScope.launch {
+            viewModel.getAccount().collectLatest { eadAccount ->
+                this@SettingsAccountFragment.eadAccount = eadAccount
+                if (eadAccount != null) {
+                    prefAdviser.isVisible = false
+                }
+                else {
+                    prefSession.summary = requireActivity().getString(R.string.log_in)
+                }
+            }
         }
-        else {
-            prefSession.summary = requireActivity().getString(R.string.log_in)
+
+        accountPreference.isBinded.observe(this) { isBinded ->
+            val eadAccount = this.eadAccount
+            if (!isBinded || eadAccount == null) return@observe
+
+            accountPreference.state.setResourceImageAndColor(R.drawable.ic_check_24,R.color.green)
+            accountPreference.userName.text = eadAccount.displayName
+            accountPreference.rank.text = eadAccount.ranksNames.toString()
+            accountPreference.profile.load(eadAccount.profileImage ?:return@observe) {
+                transformations(CircleCropTransformation())
+            }
         }
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
         return when (val key = preference.key) {
             PREFERENCE_SESSION -> {
-                if (discordUser != null) {
+                if (eadAccount != null) {
                     MaterialAlertDialogBuilder(requireContext())
                         .setTitle(getString(R.string.session))
-                        .setMessage(getString(R.string.warning_logout,discordUser.username))
+                        .setMessage(getString(R.string.warning_logout,eadAccount?.displayName))
                         .setPositiveButton(getString(R.string.confirm)) { _: DialogInterface?, _: Int ->
-                            Discord.logout()
+                            viewModel.logout()
                             val intent: Intent? = requireActivity().baseContext.packageManager.getLaunchIntentForPackage(
                                 requireActivity().baseContext.packageName
                             )
