9a56e58f4a6e855a5ad31d5adf7aff5cd94d058f
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:37:32 -0500

    removed unused functions

--- app/src/main/java/com/ead/project/dreamer/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/login/LoginViewModel.kt
@@ -1,12 +1,8 @@
 package com.ead.project.dreamer.presentation.login
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ead.project.dreamer.app.data.preference.AppBuildPreferences
 import com.ead.project.dreamer.app.model.AppBuild
-import com.ead.project.dreamer.data.models.discord.DiscordToken
-import com.ead.project.dreamer.data.models.discord.GuildMember
-import com.ead.project.dreamer.data.models.discord.DiscordUser
 import com.ead.project.dreamer.domain.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -14,7 +10,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val discordUseCase: DiscordUseCase,
     private val monosChinosUseCase: MonosChinosUseCase,
     preferenceUseCase: PreferenceUseCase
 ) : ViewModel() {
@@ -25,13 +20,4 @@ class LoginViewModel @Inject constructor(
 
     fun getApplicationState() : Flow<AppBuild> = appBuildPreferences.appBuild
 
-    fun getDiscordToken() : MutableLiveData<DiscordToken?> = discordUseCase.getDiscordUserToken.livedata()
-
-    fun getRefreshToken() : MutableLiveData<DiscordToken?> = discordUseCase.getDiscordUserRefreshToken.livedata()
-
-    fun getDiscordUser() : MutableLiveData<DiscordUser?> = discordUseCase.getDiscordUserData.livedata()
-
-    fun getDiscordUserInToGuild(id: String) : MutableLiveData<GuildMember?> = discordUseCase.getDiscordUserInToGuild.livedata(id)
-
-    fun getGuildMember(id : String) = discordUseCase.getDiscordMember.livedata(id)
 }
