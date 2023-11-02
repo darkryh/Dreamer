0329980cc6ca68e628fc2ea403cf05593b94a6fe
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:33:38 -0500

    updated properties and removed unused functions

--- app/src/main/java/com/ead/project/dreamer/data/models/discord/DiscordUser.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/discord/DiscordUser.kt
@@ -1,37 +1,30 @@
 package com.ead.project.dreamer.data.models.discord
 
 import com.ead.project.dreamer.app.data.discord.Discord
-import com.ead.project.dreamer.app.data.discord.DiscordEAD
 
 data class DiscordUser(
     val accent_color: Int,
     val avatar: String?,
+    val avatar_decoration_data: String?,
     val banner: String?,
+    val banner_color: String?,
     val discriminator: String,
     val email: String?,
     val flags: Int,
+    val global_name: String?,
     val id: String,
+    val locale: String?,
+    val mfa_enabled: Boolean,
     val premium_type: Int,
     val public_flags: Int,
     val username: String,
-    val verified: Boolean,
-    val ranks : List<String>
+    val verified: Boolean
 ) {
-
-    private val stringBuilder = StringBuilder()
-
-    val cdn_avatar get() = if (avatar != null) { "${Discord.CDN_ENDPOINT}/avatars/${id}/${avatar}" }
-    else { null }
-
-    val all_ranks : String get() = run {
-        ranks.forEach { rankId ->
-            stringBuilder.append("${DiscordEAD.rankFromId(rankId)} ")
-        }
-        val result = stringBuilder.toString()
-        stringBuilder.clear()
-        return result
+    val cdn_avatar get() = if (avatar != null) {
+        "${Discord.CDN_ENDPOINT}/avatars/${id}/${avatar}"
     }
-
-    val isVip get() = ranks.any { rank -> rank == DiscordEAD.RANK_VIP_ID }
+    else {
+        null
+    }
 
 }
