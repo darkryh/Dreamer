4d78711a37fa2cf3623f036cfbe10fe7c42aab18
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:29:58 -0500

    removed unused properties

--- app/src/main/java/com/ead/project/dreamer/domain/DiscordUseCase.kt
+++ app/src/main/java/com/ead/project/dreamer/domain/DiscordUseCase.kt
@@ -4,9 +4,5 @@ import com.ead.project.dreamer.domain.apis.discord.*
 import javax.inject.Inject
 
 class DiscordUseCase @Inject constructor(
-    val getDiscordMember: GetDiscordMember,
-    val getDiscordUserData: GetDiscordUserData,
-    val getDiscordUserInToGuild: GetDiscordUserInToGuild,
-    val getDiscordUserRefreshToken: GetDiscordUserRefreshToken,
-    val getDiscordUserToken: GetDiscordUserToken
+    val getDiscordMember: GetDiscordMember
 )
