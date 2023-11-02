265eca9d44bd4263ee49b553b9337f294ee4c123
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:23:41 -0500

    removed unused preferences

--- app/src/main/java/com/ead/project/dreamer/data/models/discord/DiscordPreference.kt
+++
@@ -1,8 +0,0 @@
-package com.ead.project.dreamer.data.models.discord
-
-data class DiscordPreference(
-    val discordUser: DiscordUser?,
-    val discordToken: DiscordToken?,
-    val accessTokenUsed : String?,
-    val exchangeCode : String?
-)
