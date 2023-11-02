0b9037ebab99b0b60f947427d3e55a5791c3d927
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:43:25 -0500

    updated properties

--- app/src/main/java/com/ead/project/dreamer/data/models/discord/UserIn.kt
+++ app/src/main/java/com/ead/project/dreamer/data/models/discord/UserIn.kt
@@ -1,9 +1,16 @@
 package com.ead.project.dreamer.data.models.discord
 
 data class UserIn(
+    val accent_color: String?,
     val avatar: String?,
+    val avatar_decoration_data: String?,
+    val banner: String?,
+    val banner_color: String?,
     val discriminator: String,
+    val flags: Int,
+    val global_name: String,
     val id: String,
     val public_flags: Int,
+    val premium_type: Int,
     val username: String
 )
