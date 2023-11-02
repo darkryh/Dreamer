3f83eabcc81654ad67831886ba06b58a4418ee15
darkryh <alex_torres-xc@hotmail.com>
2023-11-02 16:45:30 -0500

    handle discord signInDiscord

--- app/src/main/java/com/ead/project/dreamer/presentation/web/WebViewModel.kt
+++ app/src/main/java/com/ead/project/dreamer/presentation/web/WebViewModel.kt
@@ -1,16 +1,34 @@
 package com.ead.project.dreamer.presentation.web
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.ead.project.dreamer.data.models.discord.DiscordToken
+import androidx.lifecycle.viewModelScope
+import com.ead.project.dreamer.app.model.EadAccount
+import com.ead.project.dreamer.data.models.discord.SignInResult
 import com.ead.project.dreamer.domain.DiscordUseCase
+import com.ead.project.dreamer.domain.PreferenceUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WebViewModel @Inject constructor(
-    private val discordUseCase: DiscordUseCase
+    private val discordUseCase: DiscordUseCase,
+    preferenceUseCase: PreferenceUseCase
 ) : ViewModel() {
 
-    fun getToken() : MutableLiveData<DiscordToken?> = discordUseCase.getDiscordUserToken.livedata()
+    private val userPreferences = preferenceUseCase.userPreferences
+    private val _discordMember : MutableLiveData<SignInResult> = MutableLiveData()
+
+    fun signInDiscord(code : String) : LiveData<SignInResult> {
+        viewModelScope.launch {
+            _discordMember.postValue(discordUseCase.getDiscordMember(code))
+        }
+        return _discordMember
+    }
+
+    fun login(eadAccount: EadAccount) {
+        userPreferences.login(eadAccount = eadAccount)
+    }
 }
